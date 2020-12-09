aa95e4c2bcde33d04c71a43c823029a1b3acbeef
Shafiq <shafiqalshaar@gmail.com>
2020-12-09 16:09:23 +0300

    Remove random letters

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -37,7 +37,7 @@
                 <a href=""
                    class="flex bg-blue-500 hover:bg-blue-600 text-white py-5 px-10 rounded-lg text-2xl justify-center">
                     <img src="{{ asset('/img/teacher.svg') }}" class="w-8 ml-5" style="filter:invert(1)">
-                    {{ __('words.im-an-instructor') }}asd
+                    {{ __('words.im-an-instructor') }}
                 </a>
             </div>
 
