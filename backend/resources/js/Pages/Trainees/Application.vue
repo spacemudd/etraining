4c9e9b65a12414f634669276c7fbaf4e993bcfb4
Shafiq <shafiqalshaar@gmail.com>
2020-12-10 13:24:04 +0300

    Add style for upload docs

--- backend/resources/js/Pages/Trainees/Application.vue
+++ backend/resources/js/Pages/Trainees/Application.vue
@@ -18,9 +18,8 @@
             </div>
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                <form method="POST" action="https://etraining.test/register/trainees">
-                    <input type="hidden" name="_token" value="mPnEOhnihGlOOPGWCvAvNLy8TdrjC1nubSntKNVs">
-                    <div><h1 class="text-2xl text-center my-5 font-bold">اهلاً بك!</h1></div>
+                <form @submit.prevent="submitForm">
+                    <div><h1 class="text-2xl text-center my-5 font-bold">{{ $t('words.hi-there') }}</h1></div>
 
                     <div class="flex py-5 px-10 rounded-lg text-2xl justify-center">
                         <img src="https://etraining.test/img/student.svg" class="w-8 ml-5">
@@ -39,13 +38,18 @@
                                 <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
                             </svg>
                             <span class="mt-2 text-base leading-normal">{{ $t('words.select-file') }}</span>
-                            <input type='file' class="hidden" :name="required_file.name_en" />
+                            <input type='file'
+                                   class="hidden"
+                                   :ref="required_file.id"
+                                   @change="uploadFile(required_file.name_en)"
+                                   :name="required_file.name_ar"
+                                   required />
                         </label>
 
                         <!--<input class="form-input rounded-md shadow-sm block mt-1 w-full" type="text" required="required">-->
                     </div>
 
-                    <div class="flex items-center justify-end mt-4">
+                    <div class="flex items-center justify-center mt-4">
                         <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150 mx-4 tracking-normal">
                             {{ $t('words.submit') }}
                         </button>
@@ -65,7 +69,18 @@
         },
         props: [
             'required_files',
-        ]
+        ],
+        methods: {
+            form: this.$inertia.form({
+                name: this.team.name,
+            }, {
+                bag: 'uploadRequiredDocuments',
+                resetOnSuccess: false,
+            }),
+            submitForm() {
+                alert('submitting');
+            },
+        }
 
     }
 </script>
