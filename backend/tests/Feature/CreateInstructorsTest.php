bacd1e5f9335ecfb2f3bbf8fe4804570a7da792f
Shafiq <shafiqalshaar@gmail.com>
2020-12-23 12:05:19 +0300

    Refactor instructors test

--- backend/tests/Feature/CreateInstructorsTest.php
+++ backend/tests/Feature/CreateInstructorsTest.php
@@ -15,6 +15,30 @@ use Tests\TestCase;
 
 class CreateInstructorsTest extends TestCase
 {
+    public function make_instructor()
+    {
+        // Make a new team.
+        $admin = User::factory()->create();
+        $team = $admin->ownedTeams()->create([
+            'name' => 'eTraining Shafiq',
+            'personal_team' => false,
+        ]);
+        app()->make(RolesService::class)->seedRolesToTeam($team);
+
+        return [
+            'name' => 'Shafiq al-Shaar',
+            'email' => 'shafiqalshaar@gmail.com',
+            'identity_number' => '2020202010',
+            'password' => 'password',
+            'birthday' => '1994-12-10',
+            'phone' => '966565176235',
+            'phone_additional' => '966565176235',
+            'city_id' => City::create(['name' => 'Riyadh'])->id,
+            'twitter_link' => 'https://twitter.com/shafiqalshaar',
+            'provided_courses' => 'pmp',
+        ];
+    }
+
     public function test_instructors_can_see_signup_page()
     {
         $this->get(route('register.instructors'))
@@ -23,63 +47,25 @@ class CreateInstructorsTest extends TestCase
 
     public function test_instructors_register()
     {
-        // Make a new team.
-        $admin = User::factory()->create();
-        $team = $admin->ownedTeams()->create([
-            'name' => 'eTraining Shafiq',
-            'personal_team' => false,
-        ]);
-        app()->make(RolesService::class)->seedRolesToTeam($team);
+        $instructor = $this->make_instructor();
 
-        $trainee = [
-            'name' => 'Shafiq al-Shaar',
-            'email' => 'shafiqalshaar@gmail.com',
-            'identity_number' => '2020202010',
-            'password' => 'password',
-            'birthday' => '1994-12-10',
-            'phone' => '966565176235',
-            'phone_additional' => '966565176235',
-            'city_id' => City::create(['name' => 'Riyadh'])->id,
-            'twitter_link' => 'https://twitter.com/shafiqalshaar',
-            'provided_courses' => 'pmp',
-        ];
-
-        $this->post(route('register.instructors'), $trainee)
+        $this->post(route('register.instructors'), $instructor)
             ->assertSessionHasNoErrors()
             ->assertRedirect();
 
         $this->assertDatabaseHas('users', [
-            'email' => $trainee['email'],
+            'email' => $instructor['email'],
             'email_verified_at' => null,
         ]);
 
         $this->assertDatabaseHas('instructors', [
-            'email' => $trainee['email'],
+            'email' => $instructor['email'],
         ]);
     }
 
     public function test_instructor_redirected_to_completing_their_application()
     {
-        // Make a new team.
-        $admin = User::factory()->create();
-        $team = $admin->ownedTeams()->create([
-            'name' => 'eTraining Shafiq',
-            'personal_team' => false,
-        ]);
-        app()->make(RolesService::class)->seedRolesToTeam($team);
-
-        $instructor_input = [
-            'name' => 'Shafiq al-Shaar',
-            'email' => 'shafiqalshaar@gmail.com',
-            'identity_number' => '2020202010',
-            'password' => 'password',
-            'birthday' => '1994-12-10',
-            'phone' => '966565176235',
-            'phone_additional' => '966565176235',
-            'city_id' => City::create(['name' => 'Riyadh'])->id,
-            'twitter_link' => 'https://twitter.com/shafiqalshaar',
-            'provided_courses' => 'pmp',
-        ];
+        $instructor_input = $this->make_instructor();
 
         $this->post(route('register.instructors'), $instructor_input);
 
@@ -93,5 +79,12 @@ class CreateInstructorsTest extends TestCase
             })->assertPropValue('instructor_email', function ($instructor_email) use ($instructor_profile) {
                 $this->assertEquals($instructor_email, $instructor_profile->email);
             });
+    }
+
+    public function test_instructor_files_are_retrieved_when_returning_to_the_application_at_a_later_time()
+    {
+        $instructor_input = $this->make_instructor();
+        $this->post(route('register.instructors'), $instructor_input);
+        // WIP.
     }
 }
