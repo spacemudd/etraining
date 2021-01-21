cf4e9c94cc6101ce45869bd5b5f4826e10eca04c
Shafiq <shafiqalshaar@gmail.com>
2021-01-21 20:23:54 +0300

    Add Sentry loggin

--- backend/app/Exceptions/Handler.php
+++ backend/app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
@@ -33,5 +34,19 @@ class Handler extends ExceptionHandler
     public function register()
     {
         //
+    }
+
+    /**
+     *
+     * @param \App\Exceptions\Throwable $exception
+     * @throws \Throwable
+     */
+    public function report(Throwable $exception)
+    {
+        if (app()->bound('sentry') && $this->shouldReport($exception)) {
+            app('sentry')->captureException($exception);
+        }
+
+        parent::report($exception);
     }
 }
