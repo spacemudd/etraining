b24ff4432daf0d05a2b07908374fb06c4c628ea4
Shafiq <shafiqalshaar@gmail.com>
2021-01-23 23:01:29 +0300

    Start URL

---
+++ backend/database/migrations/2021_01_23_195952_add_start_url_for_zoom.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStartUrlForZoom extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('course_batch_sessions', function (Blueprint $table) {
+            $table->string('start_url')->nullable();
+            $table->string('join_url')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('course_batch_sessions', function (Blueprint $table) {
+            //
+        });
+    }
+}
