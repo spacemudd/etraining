fd4ca7dbaf190777f74f878ce8396c00fa9a5aa4
Shafiq <shafiqalshaar@gmail.com>
2021-01-24 15:30:45 +0300

    Fix namespace 2

--- backend/app/Http/Controllers/CourseBatchSessionsAttendanceController.php
+++ backend/app/Http/Controllers/CourseBatchSessionsAttendanceController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Exports\CourseBatchSessionAttendanceExport;
+use App\Back\Exports\CourseBatchSessionAttendanceExport;
 use App\Models\Back\CourseBatchSession;
 use App\Models\Back\CourseBatchSessionAttendance;
 use App\Models\Back\Trainee;
