367a87463a05fdb5fbe071f812ae49d86d1a0c9a
Shafiq <shafiqalshaar@gmail.com>
2021-01-26 16:38:12 +0300

    Correct FK setup - fixes E-TRAINING-1W

---
+++ backend/database/migrations/2021_01_26_133702_cascade_on_delete_user_id_instructors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CascadeOnDeleteUserIdInstructorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('instructors', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->foreign('user_id')->references('id')->on('users')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('instructors', function (Blueprint $table) {
+            //
+        });
+    }
+}
