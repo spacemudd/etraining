3c59aa707b5801b65009153c5f38f32b4cc91489
Shafiq <shafiqalshaar@gmail.com>
2021-02-01 15:02:09 +0300

    Another attempt at fixing the max stack call error

--- backend/resources/js/Pages/Back/CompaniesContracts/Create.vue
+++ backend/resources/js/Pages/Back/CompaniesContracts/Create.vue
@@ -4,69 +4,82 @@
             <!--<breadcrumb/>-->
 
             <div class="mt-20" v-if="company">
-                <jet-form-section @submitted.prevent="createContract">
-                    <template #title>
-                        {{ company.name_ar }}<br/>
-                        {{ company.name_en }}
-                    </template>
+                <div class="md:grid md:grid-cols-3 md:gap-6">
+                    <div class="md:col-span-1">
+                        <div class="px-4 sm:px-0">
+                            <h3 class="text-lg font-medium text-gray-900">
+                                {{ company.name_ar }}<br/>
+                                {{ company.name_en }}
+                            </h3>
+                            <p class="mt-1 text-sm text-gray-600">
+                                {{ $t('words.upload-new-contract') }}
+                            </p>
+                        </div>
+                    </div>
 
-                    <template #description>
-                    {{ $t('words.upload-new-contract') }}
-                    </template>
+                    <div class="mt-5 md:mt-0 md:col-span-2">
+                        <form @submit.prevent="createContract">
+                            <div class="shadow overflow-hidden sm:rounded-md">
+                                <div class="px-4 py-5 bg-white sm:p-6">
+                                    <div class="grid grid-cols-4 gap-6">
+                                        <template v-for="fieldName in [
+                                                'reference_number',
+                                                'contract_starts_at',
+                                                'contract_period_in_months',
+                                                'trainees_count',
+                                                'trainee_salary',
+                                                'instructor_cost',
+                                                'company_reimbursement',
+                                                ]">
+                                            <div class="col-span-2 sm:col-span-2">
+                                                <jet-label :for="fieldName" :value="$t('words.'+fieldName)" />
+                                                <jet-input :id="fieldName"
+                                                           :type="fieldName==='contract_starts_at' ? 'date' : 'text'"
+                                                           class="mt-1 block w-full"
+                                                           v-model="contractForm[fieldName]"
+                                                           autocomplete="off"
+                                                           :placeholder="fieldName==='reference_number' ? $t('words.leave-blank-to-be-automatically-generated') : ''"
+                                                           :autofocus="fieldName==='reference_number'"
+                                                           :required="fieldName === 'contract_starts_at'"
+                                                />
+                                                <jet-input-error :message="contractForm.error(fieldName)" class="mt-2" />
+                                            </div>
+                                        </template>
 
-                    <template #form>
-                        <template v-for="fieldName in [
-                            'reference_number',
-                            'contract_starts_at',
-                            'contract_period_in_months',
-                            'trainees_count',
-                            'trainee_salary',
-                            'instructor_cost',
-                            'company_reimbursement',
-                            ]">
-                            <div class="col-span-2 sm:col-span-2">
-                                <jet-label :for="fieldName" :value="$t('words.'+fieldName)" />
-                                <jet-input :id="fieldName"
-                                           :type="fieldName==='contract_starts_at' ? 'date' : 'text'"
-                                           class="mt-1 block w-full"
-                                           v-model="contractForm[fieldName]"
-                                           autocomplete="off"
-                                           :placeholder="fieldName==='reference_number' ? $t('words.leave-blank-to-be-automatically-generated') : ''"
-                                           :autofocus="fieldName==='reference_number'"
-                                           :required="fieldName === 'contract_starts_at'"
-                                />
-                                <jet-input-error :message="contractForm.error(fieldName)" class="mt-2" />
+                                        <div class="col-span-4 sm:col-span-4">
+                                            <jet-label for="notes" :value="$t('words.notes')" />
+                                            <jet-textarea id="notes" type="textarea" class="mt-1 block w-full" v-model="contractForm.notes" />
+                                            <jet-input-error :message="contractForm.error('notes')" class="mt-2" />
+                                        </div>
+
+                                        <div class="col-span-4 sm:col-span-4">
+                                            <vue-dropzone ref="dropZoneContainer"
+                                                          id="dropzone"
+                                                          @vdropzone-file-added="fileAdded"
+                                                          :options="dropzoneOptions"
+                                            ></vue-dropzone>
+                                        </div>
+                                        <!--Done form-->
+
+                                        <div class="flex items-center justify-start px-4 py-3 bg-gray-50 text-right">
+                                            <jet-action-message :on="contractForm.recentlySuccessful" class="mr-3">
+                                                {{ $t('words.created-successfully') }}
+                                            </jet-action-message>
+
+                                            <inertia-link :href="`/back/companies/${company.id}`" class="flex items-center justify-start rtl:ml-4 ltr:mr-4 rounded-md px-4 py-2 bg-white hover:bg-gray-300 text-right">
+                                                {{ $t('words.cancel') }}
+                                            </inertia-link>
+
+                                            <jet-button :class="{ 'opacity-25': contractForm.processing }" :disabled="contractForm.processing">
+                                                {{ $t('words.save') }}
+                                            </jet-button>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                        </template>
-                        <div class="col-span-4 sm:col-span-4">
-                            <jet-label for="notes" :value="$t('words.notes')" />
-                            <jet-textarea id="notes" type="textarea" class="mt-1 block w-full" v-model="contractForm.notes" />
-                            <jet-input-error :message="contractForm.error('notes')" class="mt-2" />
-                        </div>
-
-                        <div class="col-span-4 sm:col-span-4">
-                            <vue-dropzone ref="dropZoneContainer"
-                                          id="dropzone"
-                                          @vdropzone-file-added="fileAdded"
-                                          :options="dropzoneOptions"
-                            ></vue-dropzone>
-                        </div>
-                    </template>
-
-                    <template #actions>
-                        <jet-action-message :on="contractForm.recentlySuccessful" class="mr-3">
-                            {{ $t('words.created-successfully') }}
-                        </jet-action-message>
-
-                        <inertia-link :href="`/back/companies/${company.id}`" class="flex items-center justify-start rtl:ml-4 ltr:mr-4 rounded-md px-4 py-2 bg-white hover:bg-gray-300 text-right">
-                            {{ $t('words.cancel') }}
-                        </inertia-link>
-
-                        <jet-button :class="{ 'opacity-25': contractForm.processing }" :disabled="contractForm.processing">
-                            {{ $t('words.save') }}
-                        </jet-button>
-                    </template>
-                </jet-form-section>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </app-layout>
