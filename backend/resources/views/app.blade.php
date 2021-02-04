f1101a3273a10719c8664301b30e8d378461efef
Shafiq <shafiqalshaar@gmail.com>
2021-02-04 13:10:27 +0300

    Add favicon

--- backend/resources/views/app.blade.php
+++ backend/resources/views/app.blade.php
@@ -17,6 +17,11 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
         @routes
