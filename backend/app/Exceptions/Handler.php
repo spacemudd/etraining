63688bb10b1103775d352b8f3e904c9b08810fdf
Shafiq <shafiqalshaar@gmail.com>
2021-02-22 10:31:50 +0300

    Disable reporting validation errors

--- backend/app/Exceptions/Handler.php
+++ backend/app/Exceptions/Handler.php
@@ -18,24 +18,6 @@ use Throwable;
 class Handler extends ExceptionHandler
 {
     /**
-     * A list of the internal exception types that should not be reported.
-     *
-     * @var string[]
-     */
-    protected $internalDontReport = [
-        AuthenticationException::class,
-        AuthorizationException::class,
-        HttpException::class,
-        HttpResponseException::class,
-        ModelNotFoundException::class,
-        MultipleRecordsFoundException::class,
-        RecordsNotFoundException::class,
-        SuspiciousOperationException::class,
-        // TokenMismatchException::class,
-        // ValidationException::class,
-    ];
-
-    /**
      * A list of the exception types that are not reported.
      *
      * @var array
