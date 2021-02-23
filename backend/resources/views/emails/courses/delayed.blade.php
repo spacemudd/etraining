d8cdea0bcfe20bbe0e405ec6d0782c0716db3ab8
Shafiq <shafiqalshaar@gmail.com>
2021-02-23 16:40:06 +0300

    Delayed message

---
+++ backend/resources/views/emails/courses/delayed.blade.php
@@ -0,0 +1,9 @@
+@component('mail::message')
+<p style="text-align: center">
+تم تأجيل الدورة الى الغد الساعة 10
+</p>
+
+<p style="text-align: center">
+وشاكرين لكم صبركم وحسن تعاونكم
+</p>
+@endcomponent
