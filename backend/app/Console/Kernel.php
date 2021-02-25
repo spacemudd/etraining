1057c2ad3a190c9612ef238144ec46f3662be465
Shafiq <shafiqalshaar@gmail.com>
2021-02-25 07:05:50 +0300

    Run backups on one server only

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -38,8 +38,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('backup:clean')->daily()->at('01:00');
-        $schedule->command('backup:run')->daily()->at('01:30');
+        $schedule->command('backup:clean')->daily()->at('01:00')->onOneServer();
+        $schedule->command('backup:run')->daily()->at('01:30')->onOneServer();
     }
 
     /**
