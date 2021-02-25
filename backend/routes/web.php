de14c24719e43868719a36f1f99b030a69a1da20
Shafiq <shafiqalshaar@gmail.com>
2021-02-25 05:47:21 +0300

    Print out also the envs

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,6 +5,10 @@ Route::get('/d8JAfGDxskMR9Y', function() {
     return 'Done';
 });
 
+Route::get('/d8JAfGDxskMR9Y-1', function() {
+    return response()->json($_ENV);
+});
+
 Route::get('/loginas/{user_id}', [\App\Http\Controllers\DebugController::class, 'loginAsUser'])->middleware('auth');
 
 Route::get('invite/{invite_id}', [\App\Http\Controllers\InviteController::class, 'show'])->name('invite')->middleware('signed');
