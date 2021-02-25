e9904ead57191d80a9f449d3f5244ee8dfe29ef2
Shafiq <shafiqalshaar@gmail.com>
2021-02-25 03:25:13 +0300

    Add supervisord settings

--- backend/app/Http/Middleware/VerifyCsrfToken.php
+++ backend/app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,6 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         '/api/trainees/uploadcv',
-        '/login',
+        '/login', //  For testing purposes.
     ];
 }
