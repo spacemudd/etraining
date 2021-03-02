506460379a7680703b520b24e5c379be9337312b
Shafeeq Shaar <shafeeq.shaar+ci@cloudearth.com>
2021-03-02 17:03:57 +0300

    Change docker to docker-compose

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -1,5 +1,5 @@
 docker login -u AWS -p $(aws ecr get-login-password) https://$(aws sts get-caller-identity --query 'Account' --output text).dkr.ecr.eu-central-1.amazonaws.com
-docker build backend
+docker-compose build backend
 docker tag etraining_backend:latest 175380537962.dkr.ecr.eu-central-1.amazonaws.com/backend
 docker push 175380537962.dkr.ecr.eu-central-1.amazonaws.com/backend
 aws ecs update-service --cluster prod-ecs-cluster --service backend-service --force-new-deployment
