d2d66e465952086a3dbf59f8a251588231addf91
Shafeeq Shaar <shafeeq.shaar+ci@cloudearth.com>
2021-03-02 17:10:02 +0300

    Add deploy all

---
+++ deploy-all.sh
@@ -0,0 +1,21 @@
+docker login -u AWS -p $(aws ecr get-login-password) https://$(aws sts get-caller-identity --query 'Account' --output text).dkr.ecr.eu-central-1.amazonaws.com
+docker-compose build
+
+# Tagging
+docker tag etraining_backend:latest 175380537962.dkr.ecr.eu-central-1.amazonaws.com/backend
+docker tag etraining_backend-schedule:latest 175380537962.dkr.ecr.eu-central-1.amazonaws.com/schedule
+docker tag etraining_backend-web:latest 175380537962.dkr.ecr.eu-central-1.amazonaws.com/frontend
+docker tag etraining_backend-worker:latest 175380537962.dkr.ecr.eu-central-1.amazonaws.com/worker
+
+# Pushing
+docker push 175380537962.dkr.ecr.eu-central-1.amazonaws.com/backend
+docker push 175380537962.dkr.ecr.eu-central-1.amazonaws.com/schedule
+docker push 175380537962.dkr.ecr.eu-central-1.amazonaws.com/frontend
+docker push 175380537962.dkr.ecr.eu-central-1.amazonaws.com/worker
+
+# Updating cluster
+aws ecs update-service --cluster prod-ecs-cluster --service backend-service --force-new-deployment
+aws ecs update-service --cluster prod-ecs-cluster --service scheduler --force-new-deployment
+aws ecs update-service --cluster prod-ecs-cluster --service frontend-service --force-new-deployment
+aws ecs update-service --cluster prod-ecs-cluster --service worker-service --force-new-deployment
+
--- deploy-backend-web.sh
+++ deploy-backend-web.sh
@@ -1,5 +1,5 @@
 docker login -u AWS -p $(aws ecr get-login-password) https://$(aws sts get-caller-identity --query 'Account' --output text).dkr.ecr.eu-central-1.amazonaws.com
 docker-compose build backend-web
-docker tag etraining_backend:latest 175380537962.dkr.ecr.eu-central-1.amazonaws.com/backend
+docker tag etraining_backend-web:latest 175380537962.dkr.ecr.eu-central-1.amazonaws.com/frontend
 docker push 175380537962.dkr.ecr.eu-central-1.amazonaws.com/frontend
 aws ecs update-service --cluster prod-ecs-cluster --service frontend-service --force-new-deployment
