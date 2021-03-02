29bde882b80d6c4b694b4c054b31110134a38f08
Shafeeq Shaar <shafeeq.shaar+ci@cloudearth.com>
2021-03-02 15:54:00 +0300

    Remove local DB backup

--- backend/config/backup.php
+++ backend/config/backup.php
@@ -183,7 +183,7 @@ return [
     'monitor_backups' => [
         [
             'name' => env('APP_NAME', 'laravel-backup'),
-            'disks' => ['local', 's3'],
+            'disks' => ['s3'],
             'health_checks' => [
                 \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumAgeInDays::class => 1,
                 \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumStorageInMegabytes::class => 50,
