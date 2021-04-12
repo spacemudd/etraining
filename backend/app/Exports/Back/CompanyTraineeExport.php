85904aa75b740ed178d9a0474016e514712012b7
Shafiq <shafiqalshaar@gmail.com>
2021-04-12 16:26:38 +0300

    Set Excel view for trainees export

--- backend/app/Exports/Back/CompanyTraineeExport.php
+++ backend/app/Exports/Back/CompanyTraineeExport.php
@@ -3,50 +3,92 @@
 namespace App\Exports\Back;
 
 use App\Models\Back\Trainee;
-use Maatwebsite\Excel\Concerns\FromCollection;
-use Maatwebsite\Excel\Concerns\WithHeadings;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Events\AfterSheet;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class CompanyTraineeExport implements FromCollection, WithHeadings
+class CompanyTraineeExport implements FromView, WithEvents, WithStyles
 {
+    public $company_id;
 
-    public function __construct($company_id) {
+    public function __construct(string $company_id)
+    {
         $this->company_id = $company_id;
     }
 
-    /**
-    * @return \Illuminate\Support\Collection
-    */
-    public function collection() {
-        return Trainee::where('company_id', $this->company_id)->get();
+    public function registerEvents(): array
+    {
+        if (app()->getLocale() === 'ar') {
+            return [
+                AfterSheet::class    => function(AfterSheet $event) {
+                    $event->sheet->getDelegate()->setRightToLeft(true);
+                },
+            ];
+        } else {
+            return [
+                AfterSheet::class    => function(AfterSheet $event) {
+                    $event->sheet->getDelegate()->setRightToLeft(false);
+                },
+            ];
+        }
     }
 
-    public function headings(): array {
+    public function styles(Worksheet $sheet)
+    {
         return [
-            'id',
-            'team_id',
-            'user_id',
-            'name',
-            'email',
-            'identity_number',
-            'phone',
-            'phone_additional',
-            'birthday',
-            'educational_level_id',
-            'city_id',
-            'marital_status_id',
-            'children_count',
-            'company_id',
-            'deleted_at',
-            'created_at',
-            'updated_at',
-            'instructor_id',
-            'status',
-            'approved_by_id',
-            'approved_at',
-            'deleted_remark',
-            'skip_uploading_id',
+            'A:Z' => [
+                'font' => [
+                    'name' => 'Arial',
+                    'size' => 12,
+                ]
+            ],
         ];
     }
 
+    /**
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function view(): View
+    {
+        $q = Trainee::query()->where('company_id', $this->company_id);
 
+        $trainees = $q->with('company')->get()->toBase();
+
+        return view('exports.trainees', [
+            'trainees' => $trainees,
+        ]);
+    }
+
+    //public function headings(): array
+    //{
+    //    return [
+    //        'id',
+    //        'team_id',
+    //        'user_id',
+    //        'name',
+    //        'email',
+    //        'identity_number',
+    //        'phone',
+    //        'phone_additional',
+    //        'birthday',
+    //        'educational_level_id',
+    //        'city_id',
+    //        'marital_status_id',
+    //        'children_count',
+    //        'company_id',
+    //        'deleted_at',
+    //        'created_at',
+    //        'updated_at',
+    //        'instructor_id',
+    //        'status',
+    //        'approved_by_id',
+    //        'approved_at',
+    //        'deleted_remark',
+    //        'skip_uploading_id',
+    //    ];
+    //}
 }
