00bc655fec101753eb2eba1351b89b060cd89238
Shafiq alShaar <shafiqalshaar@gmail.com>
2021-08-11 12:59:16 +0300

    Update deploy code

--- deploy-all.sh
+++ deploy-all.sh
@@ -19,4 +19,6 @@ aws ecs update-service --cluster prod-ecs-cluster --service worker-service --for
 aws ecs update-service --cluster prod-ecs-cluster --service scheduler --force-new-deployment
 aws ecs update-service --cluster prod-ecs-cluster --service frontend-service --force-new-deployment
 
+aws ecs update-service --cluster prod-ecs-cluster --service backend-service --force-new-deployment
+aws ecs update-service --cluster prod-ecs-cluster --service worker-service --force-new-deployment
 docker image prune --force
--- deploy-backend.sh
+++ deploy-backend.sh
@@ -1,5 +1,9 @@
 docker login -u AWS -p $(aws ecr get-login-password) https://$(aws sts get-caller-identity --query 'Account' --output text).dkr.ecr.eu-central-1.amazonaws.com
-docker-compose build backend
+docker-compose build backend backend-worker
 docker tag etraining_backend:latest 175380537962.dkr.ecr.eu-central-1.amazonaws.com/backend
+docker tag etraining_backend-worker:latest 175380537962.dkr.ecr.eu-central-1.amazonaws.com/worker
 docker push 175380537962.dkr.ecr.eu-central-1.amazonaws.com/backend
+docker push 175380537962.dkr.ecr.eu-central-1.amazonaws.com/worker
 aws ecs update-service --cluster prod-ecs-cluster --service backend-service --force-new-deployment
+aws ecs update-service --cluster prod-ecs-cluster --service worker-service --force-new-deployment
+docker image prune --force
