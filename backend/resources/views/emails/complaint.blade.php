845c317b9461a92640e700c05dd2e4bd5f518b66
Shafiq alShaar <shafiqalshaar@gmail.com>
2021-10-06 13:46:47 +0300

    WIP

--- backend/resources/views/emails/complaint.blade.php
+++ backend/resources/views/emails/complaint.blade.php
@@ -4,13 +4,13 @@
     <meta charset="utf-8">
 </head>
 <body style="text-align:right;">
-<h5>اسم الدورة:</h5>
+<h5>:اسم الدورة</h5>
 <p>{{ $complaint->course_name }}</p>
-<h5>اسم المتدربة:</h5>
+<h5>:اسم المتدربة</h5>
 <p>{{ $complaint->course_instructor }}</p>
-<h5>الرسالة:</h5>
+<h5>:الرسالة</h5>
 <p>{{ $complaint->message }}</p>
-<h5>من قبل:</h5>
+<h5>:من قبل</h5>
 <p>{{ $complaint->user->name }} // {{ $complaint->user->phone }} // {{ $complaint->user->email }}</p>
 </body>
 </html>
