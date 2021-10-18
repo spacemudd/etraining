baab21023dc54978d34220bd5dd27f585f18eb23
Shafeeq <shafeeq.shaar@cloudearth.com>
2021-10-18 12:57:30 +0300

    Moved up

--- backend/resources/js/Pages/Obligations/Index.vue
+++ backend/resources/js/Pages/Obligations/Index.vue
@@ -3,9 +3,28 @@
         <div class="container px-6 mx-auto grid">
             <div class="grid grid-cols-1 gap-6 mt-5">
                 <div class="col text-center">
-                    <div class="px-20 text-center mx-auto">
-                        <img src="/img/obligations1.jpg" class="shadow-lg rounded-lg w-1/2 mx-auto" alt="PTCObligations">
-                    </div>
+                    <p class="text-red-500">الدعم الفني</p>
+                    <p class="text-red-500">المساعدة الفنية والتعليمية للمدربين والمتدربين</p>
+                    <br/>
+                    <p>طوال مدة تنفيذ البرنامج.</p>
+                    <br/>
+                    <p>يتعهد مركز احترافية التدريب بتقديم المساعدة الفنية والتعليمية للمدربين والمتدربين<br/> طوال مدة تنفيذ البرامج وذلك عن طريق:</p>
+                    <br/>
+                    <p>التواجد المستمر في غرفة المحاضرات للدعم الفني واللوجستي خلال فترة التدريب.</p>
+                    <br/>
+                    <p>التواصل مع المدرب والمتدرب من خلال قنوات الاتصال</p>
+                    <br/>
+                    <p>جوال</p>
+                    <p>0555026885</p>
+                    <br/>
+                    <p>أو من خلال الواتس آب</p>
+                    <a class="mt-10" href="https://api.whatsapp.com/send?phone=+966555094487">
+                        <ion-icon name="logo-whatsapp" class="w-10 h-10 text-green-600"></ion-icon>
+                    </a>
+                    <br/>
+                    <br/>
+                    <p>تقديم الدعم من خلال الايميل الخاص بمركز احترافية المدرب</p>
+                    <p>training@ptc-ksa.com</p>
                 </div>
             </div>
 
