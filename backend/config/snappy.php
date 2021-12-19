af36d1449725890118af6510334b4dab2c36ba12
Shafiq <shafiqalshaar@gmail.com>
2021-12-19 11:32:10 +0300

    Fix wkhtmltopdf default path

--- backend/config/snappy.php
+++ backend/config/snappy.php
@@ -35,7 +35,7 @@ return [
 
     'pdf' => [
         'enabled' => true,
-        'binary'  => env('WKHTML_PDF_BINARY', '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf"'),
+        'binary'  => env('WKHTML_PDF_BINARY', base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64')),
         'timeout' => false,
         'options' => [],
         'env'     => [],
