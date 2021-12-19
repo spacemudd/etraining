986a640f319e1d14ef78d02ed98cba2ae548eb41
Shafiq <shafiqalshaar@gmail.com>
2021-12-19 11:37:19 +0300

    WIP report PDF

--- backend/resources/views/pdf/trainees/attendance-sheet.blade.php
+++ backend/resources/views/pdf/trainees/attendance-sheet.blade.php
@@ -7,18 +7,21 @@
 <body class="font-sans antialiased  bg-gray-50 dark:bg-gray-900">
 <div>
     <table class="table">
+        <colgroup>
+            <col style="width:200px;">
+        </colgroup>
         <tbody>
         <tr>
-            <td>{{ $trainee->name }}</td>
             <td>الاسم</td>
+            <td>{{ $trainee->name }}</td>
         </tr>
         <tr>
-            <td>{{ $trainee->email }}</td>
             <td>الإيميل</td>
+            <td>{{ $trainee->email }}</td>
         </tr>
         <tr>
-            <td>{{ $trainee->phone }}</td>
             <td>الجوال</td>
+            <td>{{ $trainee->phone }}</td>
         </tr>
         </tbody>
     </table>
