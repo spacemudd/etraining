2720a69a901bb32eaadd2e9ada653761f6b73d2b
Shafiq <shafiqalshaar@gmail.com>
2022-02-28 09:04:22 +0300

    Change title of company

--- backend/resources/lang/ar/words.php
+++ backend/resources/lang/ar/words.php
@@ -477,7 +477,7 @@ return [
     'transaction-date' => 'تاريخ العملية',
     'training-fees' => 'رسوم التدريب',
     'please-login-to-your-account-pay-the-costs' => 'الرجاء الدخول على حسابك لعرض الرسوم التدريبية المستحقة',
-    'training-fees-have-been-issued-for-your-account-ptc' => 'تم إصدار رسوم التدريب لحسابك - مركز احترافية التدريب',
+    'training-fees-have-been-issued-for-your-account-ptc' => 'تم إصدار رسوم التدريب لحسابك - مركز احترافية المدرب',
     'mark-as-unpaid' => 'لم يتم الدفع',
     'chase' => 'التحصيل',
     'finance-audit-required' => 'مطلوب موافقة المالية',
