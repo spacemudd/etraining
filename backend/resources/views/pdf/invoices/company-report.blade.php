fc8f314d93b222b2fc620d0dd43fc41dd7be07e2
Shafiq <shafiqalshaar@gmail.com>
2022-03-13 12:32:54 +0300

    WIP

--- backend/resources/views/pdf/invoices/company-report.blade.php
+++ backend/resources/views/pdf/invoices/company-report.blade.php
@@ -9,10 +9,10 @@
 <body>
 <div class="container">
     <div class="row">
-        <div class="col-6">
+        <div class="col-6" style="text-align: right;">
             <h1>{{ __('words.account-statement') }}</h1>
         </div>
-        <div class="col-6" style="text-align:left;">
+        <div class="col-6" style="text-align:right;">
             <img src="{{ public_path('/img/ptc_invoice_logo.png')}}" alt="logo" width="200"/>
         </div>
     </div>
