c50deb981b3d21b76c4d7044328291194eb89ba3
Shafiq <shafiqalshaar@gmail.com>
2022-03-13 12:45:46 +0300

    WIP

--- backend/resources/views/pdf/invoices/company-report.blade.php
+++ backend/resources/views/pdf/invoices/company-report.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html dir="rtl">
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <link rel="stylesheet" href="{{ url('pdf.css') }}" media="screen">
@@ -9,47 +9,45 @@
 <body>
 <div class="container">
     <div class="row">
-        <div class="col-6" style="text-align: right;">
-            <h1>{{ __('words.account-statement') }}</h1>
-        </div>
-        <div class="col-6" style="text-align:right;">
+        <div class="col-6" style="text-align:left;">
             <img src="{{ public_path('/img/ptc_invoice_logo.png')}}" alt="logo" width="200"/>
+        </div>
+        <div class="col-6">
+            <h1>{{ __('words.account-statement') }}</h1>
         </div>
     </div>
 
     <div class="row">
-        <div class="col-12" style="direction: rtl">
-            <table class="table" style="text-align: right;width:100%;">
-                <colgroup>
-                    <col style="width:30%;">
-                    <col style="width:70%">
-                </colgroup>
-                <tbody>
-                <tr>
-                    <td style="text-align:right;">{{ __('words.company') }}</td>
-                    <td style="text-align:right;">{{ trim($company->resource_label) }}</td>
-                </tr>
-                <tr>
-                    <td style="text-align:right;">{{__('words.created-by')}}</td>
-                    <td style="text-align:right;">{{ optional($invoice_group->created_by)->name }}</td>
-                </tr>
-                <tr>
-                    <td style="text-align:right;">{{ __('words.date-period') }}</td>
-                    <td style="text-align:right;">
+        <table class="table">
+            <colgroup>
+                <col style="width:200px;">
+                <col style="width:300px;">
+            </colgroup>
+            <tbody>
+            <tr>
+                <td>{{ __('words.company') }}</td>
+                <td>{{ $company->resource_label }}</td>
+            </tr>
+            <tr>
+                <td>{{__('words.created-by')}}</td>
+                <td>{{ optional($invoice_group->created_by)->name }}</td>
+            </tr>
+            <tr>
+                <td>{{ __('words.date-period') }}</td>
+                <td>
                     {{ $invoice_group->from_date->toDateString() }}
                     <br>
                     {{ $invoice_group->to_date->toDateString() }}
-                    </td>
-                </tr>
-                <tr>
-                    <td style="text-align:right;">{{__('words.grand-total')}}</td>
-                    <td style="text-align:right;">{{ number_format($invoice_group->grand_total, 2) }}</td>
-                </tr>
-                </tbody>
-            </table>
+                </td>
+            </tr>
+            <tr>
+                <td>{{__('words.grand-total')}}</td>
+                <td>{{ number_format($invoice_group->grand_total, 2) }}</td>
+            </tr>
+            </tbody>
+        </table>
 
-            <p dir="ltr" class="right" style="text-align: right;">{{ __('words.print-date') }}: {{ now()->toDateTimeString() }}</p>
-        </div>
+        <p dir="ltr" class="right">{{ __('words.print-date') }}: {{ now()->toDateTimeString() }}</p>
 
         <hr>
 
