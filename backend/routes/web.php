5b3dd7a47e57633ec810cabe0269e3edb0589068
Shafiq <shafiqalshaar@gmail.com>
2022-03-14 14:27:33 +0300

    Add report

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -88,36 +88,36 @@ Route::get('zzwarnings', function() {
 });
 
 Route::get('ttreport', function() {
-    $trainees = Trainee::where('company_id', '!=', null)
-        ->where('suspended_at', null)
-        ->where('deleted_remark', null)
-        ->where('trainee_group_id', '!=', null)
-        ->where('deleted_at', null)
-        ->with('company')
-        ->whereHas('company', function($q) {$q->where('deleted_at', null);})
-        ->with([
-            'absences_06to10',
-        ])
-        ->withCount([
-            'absences_06to10',
-        ])
-        ->get();
+    //$trainees = Trainee::where('company_id', '!=', null)
+    //    ->where('suspended_at', null)
+    //    ->where('deleted_remark', null)
+    //    ->where('trainee_group_id', '!=', null)
+    //    ->where('deleted_at', null)
+    //    ->with('company')
+    //    ->whereHas('company', function($q) {$q->where('deleted_at', null);})
+    //    ->with([
+    //        'absences_06to10',
+    //    ])
+    //    ->withCount([
+    //        'absences_06to10',
+    //    ])
+    //    ->get();
+    //
+    //$traineeData = [];
+    //
+    //foreach ($trainees as $trainee) {
+    //    $traineeData[] = [
+    //        'name' => $trainee->name,
+    //        'company' => optional($trainee->company)->name_ar,
+    //        'email' => $trainee->email,
+    //        'phone' => $trainee->phone,
+    //        'instructor' => optional($trainee->instructor)->name,
+    //        'group' => optional($trainee->trainee_group)->name,
+    //        'absences_06to10' => $trainee->absences_06to10_count,
+    //    ];
+    //}
 
-    $traineeData = [];
-
-    foreach ($trainees as $trainee) {
-        $traineeData[] = [
-            'name' => $trainee->name,
-            'company' => optional($trainee->company)->name_ar,
-            'email' => $trainee->email,
-            'phone' => $trainee->phone,
-            'instructor' => optional($trainee->instructor)->name,
-            'group' => optional($trainee->trainee_group)->name,
-            'absences_06to10' => $trainee->absences_06to10_count,
-        ];
-    }
-
-    //return \Maatwebsite\Excel\Facades\Excel::download(new \App\Exports\TtTraineeReport(), now()->format('Y-m-d-h-i').'-absences.xlsx');
+    return \Maatwebsite\Excel\Facades\Excel::download(new \App\Exports\TtTraineeReport(), now()->format('Y-m-d-h-i').'-absences.xlsx');
 
     return $traineeData;
 });
