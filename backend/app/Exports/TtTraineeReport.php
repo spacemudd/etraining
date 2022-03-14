b272c9373c916061088f7265bac853a5ef188390
Shafiq <shafiqalshaar@gmail.com>
2022-03-14 14:19:30 +0300

    Add report

---
+++ backend/app/Exports/TtTraineeReport.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\FromCollection;
+
+class TtTraineeReport implements FromCollection
+{
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function collection()
+    {
+        $trainees = Trainee::where('company_id', '!=', null)
+            ->where('suspended_at', null)
+            ->where('deleted_remark', null)
+            ->where('trainee_group_id', '!=', null)
+            ->where('deleted_at', null)
+            ->with('company')
+            ->whereHas('company', function($q) {$q->where('deleted_at', null);})
+            ->with([
+                'absences_06to10',
+            ])
+            ->withCount([
+                'absences_06to10',
+            ])
+            ->get();
+
+        $traineeData = [];
+
+        foreach ($trainees as $trainee) {
+            $traineeData[] = [
+                'name' => $trainee->name,
+                'company' => optional($trainee->company)->name_ar,
+                'email' => $trainee->email,
+                'phone' => $trainee->phone,
+                'instructor' => optional($trainee->instructor)->name,
+                'group' => optional($trainee->trainee_group)->name,
+                'absences_06to10' => $trainee->absences_06to10_count,
+            ];
+        }
+
+        return $traineeData;
+    }
+}
