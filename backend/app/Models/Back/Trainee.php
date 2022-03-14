40bc29de04bbd0cbf1933703f0a48fe77f919f94
Shafiq <shafiqalshaar@gmail.com>
2022-03-14 14:27:10 +0300

    Add report

--- backend/app/Models/Back/Trainee.php
+++ backend/app/Models/Back/Trainee.php
@@ -434,7 +434,7 @@ class Trainee extends Model implements HasMedia, SearchableLabels, Auditable
     {
         return $this->hasMany(AttendanceReportRecordWarning::class)
             ->whereBetween('created_at', [
-                now()->setDate(2022, 3, 6)->startOfDay(),
+                now()->setDate(2022, 2, 6)->startOfDay(),
                 now()->setDate(2022, 3, 10)->endOfDay(),
             ]);
     }
