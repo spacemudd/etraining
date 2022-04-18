b5052eeace503929f1e36dc2af76e3ab383468a9
Shafiq <shafiqalshaar@gmail.com>
2022-04-18 15:24:28 +0300

    Fix selecting company

--- backend/resources/js/Pages/Back/Finance/Invoices/Excel.vue
+++ backend/resources/js/Pages/Back/Finance/Invoices/Excel.vue
@@ -27,14 +27,14 @@
                     <div class="col-span-12 sm:col-span-6 mt-5">
                         <jet-label class="mb-2" for="course_id" :value="$t('words.company')" />
                         <div class="relative">
-                            <datalist class="block appearance-none w-full border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            <select class="block appearance-none w-full border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                                     v-model="form.company_id"
                                     name="company_id"
                                     id="company_id">
                                 <option v-for="company in companies" :key="company.id" :value="company.id">
                                     {{ company.name_ar }}
                                 </option>
-                            </datalist>
+                            </select>
                             <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
                             </div>
