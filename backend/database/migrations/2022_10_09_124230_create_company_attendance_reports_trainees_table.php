ff8e13d9c4658b9a6ce5638c2055ac6b0c86625d
Shafiq al-Shaar <sean.spilot@gmail.com>
2022-10-11 09:54:55 +0300

    Lower foreign-key length name

--- backend/database/migrations/2022_10_09_124230_create_company_attendance_reports_trainees_table.php
+++ backend/database/migrations/2022_10_09_124230_create_company_attendance_reports_trainees_table.php
@@ -16,9 +16,9 @@ class CreateCompanyAttendanceReportsTraineesTable extends Migration
         Schema::create('company_attendance_reports_trainees', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('company_attendance_report_id');
-            $table->foreign('company_attendance_report_id')->references('id')->on('company_attendance_reports')->cascadeOnDelete();
+            $table->foreign('company_attendance_report_id', 'attendance_report_comp_trainee_fk')->references('id')->on('company_attendance_reports')->cascadeOnDelete();
             $table->uuid('trainee_id');
-            $table->foreign('trainee_id')->references('id')->on('trainees');
+            $table->foreign('trainee_id', 'attendance_report_trainee_fk')->references('id')->on('trainees');
             $table->boolean('active')->default(true);
             $table->timestamps();
         });
