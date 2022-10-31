178cd4c828ffc74ea5f87a9c2b264d1df232d0b5
Shafiq al-Shaar <sean.spilot@gmail.com>
2022-10-31 16:16:29 +0300

    Correctly classify rejected receipts

--- backend/app/Http/Controllers/Back/FinancialInvoicesController.php
+++ backend/app/Http/Controllers/Back/FinancialInvoicesController.php
@@ -264,8 +264,8 @@ class FinancialInvoicesController extends Controller
         ]);
 
         $invoice = Invoice::findOrFail($id);
-        $invoice->status = Invoice::STATUS_UNPAID;
-        $invoice->chased_note = $request->reason;
+        $invoice->status = Invoice::STATUS_PAYMENT_RECEIPT_REJECTED;
+        $invoice->chased_note = $request->chased_note;
         $invoice->chased_by_id = auth()->user()->id;
         $invoice->paid_at = null;
         $invoice->save();
