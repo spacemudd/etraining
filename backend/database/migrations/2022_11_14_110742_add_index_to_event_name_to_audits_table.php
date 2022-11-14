5160d9493c7b6405b169031dbe66c6aa2ef3b16c
Shafiq al-Shaar <sean.spilot@gmail.com>
2022-11-14 14:08:27 +0300

    Add index to audits table

---
+++ backend/database/migrations/2022_11_14_110742_add_index_to_event_name_to_audits_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIndexToEventNameToAuditsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('audits', function (Blueprint $table) {
+            $table->index(['event']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('audits', function (Blueprint $table) {
+            $table->dropIndex(['event']);
+        });
+    }
+}
