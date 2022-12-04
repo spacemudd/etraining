64ddb82b780fed0a7c14cb2b7e3b92522892ed1b
Shafiq al-Shaar <sean.spilot@gmail.com>
2022-12-04 16:47:42 +0300

    Trying something

--- backend/resources/views/pdf/certificate/show.blade.php
+++ backend/resources/views/pdf/certificate/show.blade.php
@@ -52,16 +52,18 @@
 <body>
     <div class="content">
         <img class="page-logo" src="{{ public_path('/img/logo-lg.png') }}">
-        <h1>شهادة حضور دورة تطويرية</h1>
+        <h1 style="margin-top:5rem;">شهادة حضور دورة تطويرية</h1>
         <p style="margin-top:2.5rem;">يشهد مركز احترافية المدرب للتدريب بأن المتدربة  "{{ $certificate->trainee->name }}"</p>
         <p style="margin-top:2.5rem;">وجنسيتها سعودية بموجب السجل المدني رقم "{{ $certificate->trainee->identity_number }}"</p>
-        <p style="margin-top:2.5rem;">قـــد حضرت دورة ({{ $certificate->course->name_ar }}) عدد ساعاتها التدريبية 32 ساعة</p>
+        <p style="margin-top:2.5rem;">قـــد حضرت دورة</p>
+        <p style="margin-top:2.5rem;">({{ $certificate->course->name_ar }})</p>
+        <p style="margin-top:2.5rem;">عدد ساعاتها التدريبية 32 ساعة</p>
         <p style="margin-top:2.5rem;">وعدد أيامها التدريبية (30) يوم والمنعقدة في الفترة من </p>
         <p style="margin-top:2.5rem;">(8 - 10 -1443هـ  إلى 6 -4 -1444هـ  )</p>
         <p style="margin-top:2.5rem;">والمعتمدة من المؤسسة العامة للتدريب التقني والمهني برقم </p>
         <p style="margin-top:2.5rem;">(192246234)</p>
         <p style="margin-top:5rem;">والله الموفق،،،</p>
-        <div class="row" style="margin-top: 7rem;">
+        <div class="row" style="margin-top: 5rem;">
             <div class="col-7">
                 <p style="margin-bottom:0;padding-bottom: 0"><b>ختم المنشأة التدريبية</b></p>
                 <img id="page-stamp" src="{{ public_path('/img/certificate-stamp.png') }}">
