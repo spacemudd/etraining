fd242f8afb69391ad44a2afb010e9dbd0650a5e6
Shafiq al-Shaar <sean.spilot@gmail.com>
2022-12-05 09:10:20 +0300

    Change course id for certificates

--- backend/app/Http/Controllers/Back/CertificatesController.php
+++ backend/app/Http/Controllers/Back/CertificatesController.php
@@ -27,7 +27,7 @@ class CertificatesController extends Controller
         $filepath = storage_path('app').'/'.$path;
 
         $import = new CertificatesImport();
-        $import->course_id = Course::find('6b83be85-0f0f-4758-9d5e-840f97c87519')->id ?? Course::first()->id;
+        $import->course_id = Course::find('41c53a38-a80a-4562-9ad7-f7ff1985a60b')->id ?? Course::first()->id;
         $import->status = CertificatesImport::STATUS_IMPORTING;
         $import->processed_count = 0;
         $import->total_count = 0;
