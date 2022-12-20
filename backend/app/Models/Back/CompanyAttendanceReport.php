28890b54ea351d2d21fee62c00e72e0560fea0a3
Samar Alali <alalisummer@gmail.com>
2022-12-20 09:31:30 +0300

    Enable soft-delete "CompanyAttendanceReport"

--- backend/app/Models/Back/CompanyAttendanceReport.php
+++ backend/app/Models/Back/CompanyAttendanceReport.php
@@ -4,10 +4,12 @@ namespace App\Models\Back;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CompanyAttendanceReport extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     const STATUS_REVIEW = 1;
     const STATUS_APPROVED = 2;
