1496660f4787e271b173ae2a81b6b965937f5c47
Samar Alali <alalisummer@gmail.com>
2023-01-02 10:33:03 +0300

    Send Certificates

--- backend/app/Http/Controllers/Back/CertificatesController.php
+++ backend/app/Http/Controllers/Back/CertificatesController.php
@@ -27,7 +27,7 @@ class CertificatesController extends Controller
         $filepath = storage_path('app').'/'.$path;
 
         $import = new CertificatesImport();
-        $import->course_id = Course::find('6b83be85-0f0f-4758-9d5e-840f97c87519')->id ?? Course::first()->id;
+        $import->course_id = Course::find('7f2b6a06-4d90-4026-97ca-a6845e90f8db')->id ?? Course::first()->id;
         $import->status = CertificatesImport::STATUS_IMPORTING;
         $import->processed_count = 0;
         $import->total_count = 0;
--- backend/resources/views/pdf/certificate/show.blade.php
+++ backend/resources/views/pdf/certificate/show.blade.php
@@ -56,10 +56,10 @@
         <p style="margin-top:2.5rem;">يشهد مركز احترافية المدرب للتدريب بأن المتدربة  "{{ $certificate->trainee->name }}"</p>
         <p style="margin-top:2.5rem;">وجنسيتها سعودية بموجب السجل المدني رقم "{{ $certificate->trainee->identity_number }}"</p>
         <p style="margin-top:2.5rem;">قـــد حضرت دورة ({{ $certificate->course->name_ar }}) عدد ساعاتها التدريبية 16 ساعة</p>
-        <p style="margin-top:2.5rem;">وعدد أيامها التدريبية (8) يوم والمنعقدة في الفترة من </p>
-        <p style="margin-top:2.5rem;">(12 - 4 -1444هـ  إلى 6 -5 -1444هـ  )</p>
+        <p style="margin-top:2.5rem;">وعدد أيامها التدريبية (4) يوم والمنعقدة في الفترة من </p>
+        <p style="margin-top:2.5rem;">(10 - 5 -1444هـ  إلى 4 - 6 -1444هـ  )</p>
         <p style="margin-top:2.5rem;">والمعتمدة من المؤسسة العامة للتدريب التقني والمهني برقم </p>
-        <p style="margin-top:2.5rem;">(192283432)</p>
+        <p style="margin-top:2.5rem;">(19229842)</p>
         <p style="margin-top:5rem;">والله الموفق،،،</p>
         <div class="row" style="margin-top: 2rem;">
             <div class="col-7">
