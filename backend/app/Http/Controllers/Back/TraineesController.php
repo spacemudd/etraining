c4b50beb9b45c00ea99150cb24a45e5e242edf13
Samar Alali <alalisummer@gmail.com>
2023-01-04 14:48:29 +0300

    WIP

--- backend/app/Http/Controllers/Back/TraineesController.php
+++ backend/app/Http/Controllers/Back/TraineesController.php
@@ -6,6 +6,7 @@ use App\Actions\Fortify\CreateNewTraineeUser;
 use App\Http\Controllers\Controller;
 use App\Jobs\ExportArchivedTraineesToExcelJob;
 use App\Jobs\ExportTraineesToExcelJob;
+use App\Mail\DeletedTraineeMail;
 use App\Mail\WarningMail;
 use App\Models\Back\AttendanceReportRecord;
 use App\Models\Back\AttendanceReportRecordWarning;
@@ -651,7 +652,7 @@ class TraineesController extends Controller
         //}
         $users = User::permission('receive-notification-on-trainee-delete')->get();
         Mail::to($users)
-            ->queue(new WarningMail($trainee, auth()->user()->email));
+            ->queue(new DeletedTraineeMail($trainee, auth()->user()->email));
 
         DB::commit();
         return redirect()->route('back.trainees.index');
@@ -898,10 +899,11 @@ class TraineesController extends Controller
         foreach ($warnings as $warning) {
             $warning->attendance_report_record->status = AttendanceReportRecord::STATUS_ABSENT;
             $warning->attendance_report_record->save();
+            if($warnings >= 3){
+                Mail::to(['sara@ptc-ksa.com', 'trainee.affairs@ptc-ksa.com'])
+                    ->queue(new WarningMail($warnings, auth()->user()->email));
+            }
         }
-        Mail::to(['sara@ptc-ksa.com', 'trainee.affairs@ptc-ksa.com'])
-            ->queue(new WarningMail($warnings, auth()->user()->email));
-
         return AttendanceReportRecordWarning::where('trainee_id', $trainee_id)
             ->with([
                 'attendance_report_record' => function ($q) {
