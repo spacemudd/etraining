cc8264d62f1f79865f5792a6bdfefccd40638f72
shafiqalshaar <shafiqalshaar@gmail.com>
2023-02-05 09:49:34 +0300

    Remove indexing of companies

--- backend/resources/js/Pages/Back/Companies/Index.vue
+++ backend/resources/js/Pages/Back/Companies/Index.vue
@@ -10,61 +10,62 @@
             <div class="flex justify-between">
                 <h1 class="mb-8 font-bold text-3xl">{{ $t('words.companies') }}</h1>
                 <div class="mb-6 flex justify-between items-center gap-2">
-                    <a class="btn-gray" :href="route('back.companies.export')" v-can="'companies-report'">
-                        <span>{{ $t('words.export') }}</span>
-                    </a>
+                    <!--<a class="btn-gray" :href="route('back.companies.export')" v-can="'companies-report'">-->
+                    <!--    <span>{{ $t('words.export') }}</span>-->
+                    <!--</a>-->
                     <inertia-link class="btn-gray" :href="route('back.companies.create')">
                         <span>{{ $t('words.new') }}</span>
                     </inertia-link>
                 </div>
             </div>
-            <div class="bg-white rounded shadow overflow-x-auto">
-                <table class="w-full whitespace-no-wrap">
-                    <tr class="text-left font-bold">
-                        <th class="px-6 pt-6 pb-4">{{ $t('words.name') }}</th>
-                        <th class="px-6 pt-6 pb-4">{{ $t('words.cr_number') }}</th>
-                        <th class="px-6 pt-6 pb-4">{{ $t('words.address') }}</th>
-                        <th class="px-6 pt-6 pb-4" colspan="2">{{ $t('words.contact_number') }}</th>
-                    </tr>
-                    <tr v-for="company in companies.data" :key="company.id" class="hover:bg-gray-100 focus-within:bg-gray-100">
-                        <td class="border-t">
-                            <inertia-link class="px-6 py-4 flex items-center focus:text-indigo-500" :href="route('back.companies.show', company.id)">
-                                {{ company.name_ar }}<br/>
-                                {{ company.name_en }}
-                                <!--<icon v-if="company.deleted_at" name="trash" class="flex-shrink-0 w-3 h-3 fill-gray-400 ml-2" />-->
-                            </inertia-link>
-                        </td>
-                        <td class="border-t">
-                            <inertia-link class="px-6 py-4 flex items-center" :href="route('back.companies.show', company.id)" tabindex="-1">
-                                <div v-if="company.cr_number">
-                                    {{ company.cr_number }}
-                                </div>
-                            </inertia-link>
-                        </td>
-                        <td class="border-t">
-                            <inertia-link class="px-6 py-4 flex items-center" :href="route('back.companies.show', company.id)" tabindex="-1">
-                                {{ company.address }}
-                            </inertia-link>
-                        </td>
-                        <td class="border-t">
-                            <inertia-link class="px-6 py-4 flex items-center" :href="route('back.companies.show', company.id)" tabindex="-1">
-                                {{ company.contact_number }}
-                            </inertia-link>
-                        </td>
-                        <td class="border-t w-px">
-                            <inertia-link class="px-4 flex items-center" :href="route('back.companies.show', company.id)" tabindex="-1">
-                                <ion-icon name="arrow-forward-outline" class="block w-6 h-6 fill-gray-400"></ion-icon>
-                            </inertia-link>
-                        </td>
-                    </tr>
-                    <tr v-if="companies.data.length === 0">
-                        <td class="border-t px-6 py-4" colspan="4">
-                            <empty-slate/>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-            <pagination :links="companies.links" />
+            <admin-searchbar/>
+            <!--<div class="bg-white rounded shadow overflow-x-auto">-->
+            <!--    <table class="w-full whitespace-no-wrap">-->
+            <!--        <tr class="text-left font-bold">-->
+            <!--            <th class="px-6 pt-6 pb-4">{{ $t('words.name') }}</th>-->
+            <!--            <th class="px-6 pt-6 pb-4">{{ $t('words.cr_number') }}</th>-->
+            <!--            <th class="px-6 pt-6 pb-4">{{ $t('words.address') }}</th>-->
+            <!--            <th class="px-6 pt-6 pb-4" colspan="2">{{ $t('words.contact_number') }}</th>-->
+            <!--        </tr>-->
+            <!--        <tr v-for="company in companies.data" :key="company.id" class="hover:bg-gray-100 focus-within:bg-gray-100">-->
+            <!--            <td class="border-t">-->
+            <!--                <inertia-link class="px-6 py-4 flex items-center focus:text-indigo-500" :href="route('back.companies.show', company.id)">-->
+            <!--                    {{ company.name_ar }}<br/>-->
+            <!--                    {{ company.name_en }}-->
+            <!--                    &lt;!&ndash;<icon v-if="company.deleted_at" name="trash" class="flex-shrink-0 w-3 h-3 fill-gray-400 ml-2" />&ndash;&gt;-->
+            <!--                </inertia-link>-->
+            <!--            </td>-->
+            <!--            <td class="border-t">-->
+            <!--                <inertia-link class="px-6 py-4 flex items-center" :href="route('back.companies.show', company.id)" tabindex="-1">-->
+            <!--                    <div v-if="company.cr_number">-->
+            <!--                        {{ company.cr_number }}-->
+            <!--                    </div>-->
+            <!--                </inertia-link>-->
+            <!--            </td>-->
+            <!--            <td class="border-t">-->
+            <!--                <inertia-link class="px-6 py-4 flex items-center" :href="route('back.companies.show', company.id)" tabindex="-1">-->
+            <!--                    {{ company.address }}-->
+            <!--                </inertia-link>-->
+            <!--            </td>-->
+            <!--            <td class="border-t">-->
+            <!--                <inertia-link class="px-6 py-4 flex items-center" :href="route('back.companies.show', company.id)" tabindex="-1">-->
+            <!--                    {{ company.contact_number }}-->
+            <!--                </inertia-link>-->
+            <!--            </td>-->
+            <!--            <td class="border-t w-px">-->
+            <!--                <inertia-link class="px-4 flex items-center" :href="route('back.companies.show', company.id)" tabindex="-1">-->
+            <!--                    <ion-icon name="arrow-forward-outline" class="block w-6 h-6 fill-gray-400"></ion-icon>-->
+            <!--                </inertia-link>-->
+            <!--            </td>-->
+            <!--        </tr>-->
+            <!--        <tr v-if="companies.data.length === 0">-->
+            <!--            <td class="border-t px-6 py-4" colspan="4">-->
+            <!--                <empty-slate/>-->
+            <!--            </td>-->
+            <!--        </tr>-->
+            <!--    </table>-->
+            <!--</div>-->
+            <!--<pagination :links="companies.links" />-->
         </div>
     </app-layout>
 </template>
@@ -81,11 +82,13 @@
     import IconNavigate from 'vue-ionicons/dist/ios-arrow-dropright'
     import BreadcrumbContainer from "@/Components/BreadcrumbContainer";
     import EmptySlate from "@/Components/EmptySlate";
+    import AdminSearchbar from '@/Components/AdminSearchbar';
 
     export default {
         metaInfo: { title: 'Companies' },
         // layout: Layout,
         components: {
+            AdminSearchbar,
             EmptySlate,
             BreadcrumbContainer,
             IconNavigate,
