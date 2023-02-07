c30ca3266d1aa595b00cce057a888671bb026c18
shafiqalshaar <shafiqalshaar@gmail.com>
2023-02-07 15:08:15 +0300

    Add GOSI check automation

---
+++ backend/app/Console/Commands/GosiCheck.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Facebook\WebDriver\Chrome\ChromeOptions;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Illuminate\Support\Str;
+use Laravel\Dusk\Browser;
+use Laravel\Dusk\Chrome\ChromeProcess;
+
+class GosiCheck extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'gosi:check';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $process = (new ChromeProcess)->toProcess();
+        $process->start();
+        $driver = $this->driver();
+
+        $browser = new Browser($driver);
+        $text = $browser->visit('https://gosi.gov.sa/ar/CheckEmploymentStatus')
+            ->waitFor('#CivilId', 30)
+            ->typeSlowly('#CivilId', '1127301214')
+            ->click('form.ng-dirty > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > a:nth-child(1)')
+            ->waitForText('الإشتراك الخاضع لانظمة التأمينات:', 30)
+            ->text('form.ng-untouched > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1)');
+
+        // TODO: Add DB columns to track of GOSI status
+        if (Str::contains($text, 'أنت على رأس العمل حالياً كمشترك في نظام التأمينات الاجتماعية ولديك مدد اشتراك سابقة')) {
+            // TODO: Update status of GOSI registration
+            // TODO: Send an email to PTC employees (with permission: receive-gosi-notifications)
+            $this->info('GOSI: Registered');
+        } else {
+            // TODO: Update status of GOSI registration
+            // TODO: Send an email to PTC employees if GOSI registration expired
+            $this->info('GOSI: Not registered');
+        }
+
+        $browser->quit();
+        $process->stop();
+
+        return 1;
+    }
+
+        /**
+     * Create the RemoteWebDriver instance.
+     *
+     * @return \Facebook\WebDriver\Remote\RemoteWebDriver
+     */
+    protected function driver()
+    {
+        $options = (new ChromeOptions)->addArguments([
+            '--window-size=1920,1080',
+        ]);
+        $capabilities = DesiredCapabilities::chrome()->setCapability(ChromeOptions::CAPABILITY, $options);
+
+        return retry(5, function () use($capabilities) {
+            return RemoteWebDriver::create('http://localhost:9515', $capabilities);
+        }, 50);
+    }
+}
