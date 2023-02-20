f5b8cd64fbe57edb56a3b2c33c8fe41e5cdfe37a
shafiqalshaar <shafiqalshaar@gmail.com>
2023-02-20 11:33:33 +0300

    Update

--- backend/app/Models/Back/Company.php
+++ backend/app/Models/Back/Company.php
@@ -60,7 +60,10 @@ class Company extends Model implements SearchableLabels, Auditable
 
         if (Str::contains(optional(auth()->user())->email, 'ptc-ksa.net')) {
             static::addGlobalScope('RiyadhBankAccounts', function (Builder $builder) {
-                $builder->whereIn('id', app()->make(CompaniesAssignedToRiyadhBank::class)->list);
+                $new_ids = Company::where('created_at', Carbon::parse('2023-02-09'))
+                    ->pluck('id');
+                $ids = array_merge($new_ids, app()->make(CompaniesAssignedToRiyadhBank::class)->list);
+                $builder->whereIn('id', $ids);
             });
         }
 
