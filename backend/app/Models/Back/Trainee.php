00c23fc89796d0f48c6f0db09af86dc7eb5c1cde
shafiqalshaar <shafiqalshaar@gmail.com>
2023-02-26 16:04:51 +0300

    WIP - Filtering ptc-ksa.net trainees

--- backend/app/Models/Back/Trainee.php
+++ backend/app/Models/Back/Trainee.php
@@ -121,7 +121,7 @@ class Trainee extends Model implements HasMedia, SearchableLabels, Auditable
         if (Str::contains(optional(auth()->user())->email, 'ptc-ksa.net')) {
             static::addGlobalScope('RiyadhBankAccounts', function (Builder $builder) {
                 $builder->whereIn('company_id', [app()->make(CompaniesAssignedToRiyadhBank::class)->getCompanies(), null])
-                    ->orWhereNotIn('company_id', app()->make(CompaniesAssignedToRiyadhBank::class)->removeFromPtcNet);
+                    ->whereNotIn('company_id', app()->make(CompaniesAssignedToRiyadhBank::class)->removeFromPtcNet);
             });
         }
 
