316ad173503f5c442cdaeea5916e37123d2f731e
shafiqalshaar <shafiqalshaar@gmail.com>
2023-02-27 18:39:44 +0300

    WIP

--- backend/resources/views/emails/company-attendance-report.blade.php
+++ backend/resources/views/emails/company-attendance-report.blade.php
@@ -1,4 +1,4 @@
-@component('mail::message_ptc_net')
+@component('mail::messageptcnet')
 # @lang('words.hi-there')!
 
 تم اصدار تقرير الحضور للمتدربات ({{ optional($report->approved_at)->setTimezone('Asia/Riyadh')->toDateString() }})، ويرجى الإطلاع على المرفق
