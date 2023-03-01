fb13eb9f66b1503f8875e50f2cee5e74aa3633c8
Samar Alali <alalisummer@gmail.com>
2023-03-01 15:02:25 +0300

    Certificate 1

--- backend/app/Http/Controllers/Back/CertificatesController.php
+++ backend/app/Http/Controllers/Back/CertificatesController.php
@@ -27,7 +27,7 @@ class CertificatesController extends Controller
         $filepath = storage_path('app').'/'.$path;
 
         $import = new CertificatesImport();
-        $import->course_id = Course::find('f0f6bed6-10fe-4f22-9abf-6421159954cb')->id ?? Course::first()->id;
+        $import->course_id = Course::find('a43b1a69-8980-45e0-a7f6-d173d3fc0d9f')->id ?? Course::first()->id;
         $import->status = CertificatesImport::STATUS_IMPORTING;
         $import->processed_count = 0;
         $import->total_count = 0;
