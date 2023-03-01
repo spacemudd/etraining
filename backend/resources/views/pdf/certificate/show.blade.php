65e77863fc4f07611791bde674b44e8c5b9402a8
Samar Alali <alalisummer@gmail.com>
2023-03-01 16:18:39 +0300

    Certificate 6

--- backend/resources/views/pdf/certificate/show.blade.php
+++ backend/resources/views/pdf/certificate/show.blade.php
@@ -56,10 +56,10 @@
         <p style="margin-top:2.5rem;">يشهد مركز احترافية المدرب للتدريب بأن المتدربة  "{{ $certificate->trainee->name }}"</p>
         <p style="margin-top:2.5rem;">وجنسيتها سعودية بموجب السجل المدني رقم "{{ $certificate->trainee->identity_number }}"</p>
         <p style="margin-top:2.5rem;">قـــد حضرت دورة ({{ $certificate->course->name_ar }}) عدد ساعاتها التدريبية 16 ساعة</p>
-        <p style="margin-top:2.5rem;">وعدد أيامها التدريبية (8) يوم والمنعقدة في الفترة من </p>
-        <p style="margin-top:2.5rem;">(29 - 5 - 1443هـ  إلى 27 - 9 - 1443هـ  )</p>
+        <p style="margin-top:2.5rem;">وعدد أيامها التدريبية (4) يوم والمنعقدة في الفترة من </p>
+        <p style="margin-top:2.5rem;">(10 - 5 - 1444هـ  إلى 4 - 6 - 1444هـ  )</p>
         <p style="margin-top:2.5rem;">والمعتمدة من المؤسسة العامة للتدريب التقني والمهني برقم </p>
-        <p style="margin-top:2.5rem;">(192214933)</p>
+        <p style="margin-top:2.5rem;">(19229842)</p>
         <p style="margin-top:5rem;">والله الموفق،،،</p>
         <div class="row" style="margin-top: 2rem;">
             <div class="col-7">
@@ -68,8 +68,8 @@
             </div>
             <div class="col-4" style="text-align: right;">
                 <p class="small-text">
-                    <b>حررت بتاريخ:    27 / 9  /1443  هـ </b><br/>
-                    <b>الموافق:            28 /  4 /2022 م</b>
+                    <b>حررت بتاريخ:    4 / 6  /1444  هـ </b><br/>
+                    <b>الموافق:            28 /  12 /2022 م</b>
                 </p>
                 <p class="small-text">ملاحظة: أي كشط او تعديل على الشهادة تعتبر لاغية</p>
             </div>
