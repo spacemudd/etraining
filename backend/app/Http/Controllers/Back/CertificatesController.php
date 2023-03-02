ad470557ca2727d5c2b7ea5993a909ebabfbc656
Samar Alali <alalisummer@gmail.com>
2023-03-02 10:33:54 +0300

    Certificate 5

--- backend/app/Http/Controllers/Back/CertificatesController.php
+++ backend/app/Http/Controllers/Back/CertificatesController.php
@@ -27,7 +27,7 @@ class CertificatesController extends Controller
         $filepath = storage_path('app').'/'.$path;
 
         $import = new CertificatesImport();
-        $import->course_id = Course::find('35550219-8b7f-4b5d-bc3a-6248617fa298')->id ?? Course::first()->id;
+        $import->course_id = Course::find('9b25311c-c763-482c-b94c-1e5a0525824f')->id ?? Course::first()->id;
         $import->status = CertificatesImport::STATUS_IMPORTING;
         $import->processed_count = 0;
         $import->total_count = 0;
--- backend/resources/views/pdf/certificate/show.blade.php
+++ backend/resources/views/pdf/certificate/show.blade.php
@@ -57,9 +57,9 @@
         <p style="margin-top:2.5rem;">وجنسيتها سعودية بموجب السجل المدني رقم "{{ $certificate->trainee->identity_number }}"</p>
         <p style="margin-top:2.5rem;">قـــد حضرت دورة ({{ $certificate->course->name_ar }}) عدد ساعاتها التدريبية 16 ساعة</p>
         <p style="margin-top:2.5rem;">وعدد أيامها التدريبية (8) يوم والمنعقدة في الفترة من </p>
-        <p style="margin-top:2.5rem;">(3 - 11 - 1442هـ  إلى 4 - 12 - 1442هـ  )</p>
+        <p style="margin-top:2.5rem;">(29 - 5 - 1443هـ  إلى 27 - 9 - 1443هـ  )</p>
         <p style="margin-top:2.5rem;">والمعتمدة من المؤسسة العامة للتدريب التقني والمهني برقم </p>
-        <p style="margin-top:2.5rem;">(192258735)</p>
+        <p style="margin-top:2.5rem;">(192214933)</p>
         <p style="margin-top:5rem;">والله الموفق،،،</p>
         <div class="row" style="margin-top: 2rem;">
             <div class="col-7">
@@ -68,8 +68,8 @@
             </div>
             <div class="col-4" style="text-align: right;">
                 <p class="small-text">
-                    <b>حررت بتاريخ:    4 / 12  /1442  هـ </b><br/>
-                    <b>الموافق:            14 /  7 /2021 م</b>
+                    <b>حررت بتاريخ:    27 / 9  /1443  هـ </b><br/>
+                    <b>الموافق:            28 /  4 /2022 م</b>
                 </p>
                 <p class="small-text">ملاحظة: أي كشط او تعديل على الشهادة تعتبر لاغية</p>
             </div>
