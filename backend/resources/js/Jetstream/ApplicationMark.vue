b22f160ae0342503ca3b227ed0c7faeff4c2679e
shafiqalshaar <shafiqalshaar@gmail.com>
2023-03-16 16:22:26 +0300

    Fix loading logo

--- backend/resources/js/Jetstream/ApplicationMark.vue
+++ backend/resources/js/Jetstream/ApplicationMark.vue
@@ -1,3 +1,23 @@
 <template>
-    <img :src="window.location.href.includes('ptc-ksa.net') ? '/img/logo.png' : '/img/logo-lg.png'" alt="Logo">
+    <img :src="link" alt="Logo">
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                link: '',
+            }
+        },
+        mounted() {
+            if (window.location.href.includes('ptc-ksa.net')) {
+                this.link = '/img/logo.png';
+            } else {
+                this.link = '/img/logo-lg.png';
+            }
+        },
+        methods: {
+            //
+        }
+    }
+</script>
