ae3d75f8610726236cbcba690cf3042d5052e410
Samar Alali <alalisummer@gmail.com>
2023-03-22 09:01:26 +0300

    Replicate Trainee groups Command

---
+++ backend/app/Console/Commands/DuplicateGroupCommand.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Back\AttendanceReportRecord;
+use App\Models\Back\Trainee;
+use App\Models\Back\TraineeGroup;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class DuplicateGroupCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'trainee:group';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'For random adhoc execution of commands';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     * @throws \Throwable
+     */
+    public function handle()
+    {
+        DB::beginTransaction();
+
+        $records = TraineeGroup::where('name', 'شعبة 2')->get();
+
+        foreach ($records as $record) {
+            $newRecord = $record->replicate();
+            $newRecord->save();
+        }
+        DB::commit();
+
+        return 1;
+
+    }
+}
