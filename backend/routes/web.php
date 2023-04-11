c1d0e60a4af7eaffb56757593c9d6883a6b66621
shafiqalshaar <shafiqalshaar@gmail.com>
2023-04-11 13:02:06 +0300

    Fix storing tap receipts

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -55,7 +55,7 @@ Route::get('login/verify-code', [\App\Http\Controllers\VerificationsController::
 Route::post('login/verify-code', [\App\Http\Controllers\VerificationsController::class, 'verifyCode'])->name('login.verify-code');
 
 
-Route::withoutMiddleware(IsDisabledWebsiteMiddleware::class)->post('tap', [\App\Http\Controllers\Trainees\Payment\PaymentCardController::class, 'storeReceipt']);
+Route::withoutMiddleware(IsDisabledWebsiteMiddleware::class)->post('tap', [\App\Http\Controllers\Trainees\Payment\PaymentCardController::class, 'storeTapReceipt']);
 
 Route::get('version', function() {
     return '4.7';
