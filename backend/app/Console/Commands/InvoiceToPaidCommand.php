995a170bf2cdea0b2a2d9910293d520113659aa7
shafiqalshaar <shafiqalshaar@gmail.com>
2023-04-11 14:01:28 +0300

    Add update

--- backend/app/Console/Commands/InvoiceToPaidCommand.php
+++ backend/app/Console/Commands/InvoiceToPaidCommand.php
@@ -37,6 +37,31 @@ class InvoiceToPaidCommand extends Command
      */
     public function handle()
     {
+        $data = [
+            'وعد سعود مسفر القحطاني' => 'chg_LV02G2020231300Xx831104316',
+            'أسماء نزال دهيم العتيبي' => 'chg_LV05G0420231300Xb381104887',
+        ];
+
+        foreach ($data as $name => $payment_reference_id) {
+            $trainee = Trainee::where('name', $name)->first();
+            if ($trainee) {
+
+                if ($trainee->invoices()->notPaid()->count() > 1) {
+                    $this->info('More than 1 invoice: '.$name);
+                    continue;
+                }
+
+                $trainee->invoices()->notPaid()->first()->update([
+                   'status' => 1,
+                    'payment_method' => 1,
+                    'payment_reference_id' => $payment_reference_id,
+                    'paid_at' => Carbon::now(),
+                    'trainee_bank_payment_receipt_id' => null,
+                ]);
+            }
+        }
+
+        //$data = ['status' => 1, 'payment_method' => 1, 'payment_reference_id' => 'chg_LV03G5420232233Ze8b1004061', 'paid_at' => '2023-04-10 10:35:00', 'trainee_bank_payment_receipt_id' => null]
         return 0;
     }
 }
