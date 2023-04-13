a0495fbd6d6271dce8ac65e642c0030ef1b974f9
shafiqalshaar <shafiqalshaar@gmail.com>
2023-04-13 20:50:24 +0300

    Setting tap key with trashed companies

--- backend/app/Services/CompanyMigrationHelper.php
+++ backend/app/Services/CompanyMigrationHelper.php
@@ -13,7 +13,7 @@ class CompanyMigrationHelper
      */
     public function setTapKey(string $company_id)
     {
-        if (Company::find($company_id)->is_ptc_net) {
+        if (Company::withTrashed()->find($company_id)->is_ptc_net) {
             config(['tap-payment.auth.api_key' => env('TAP_PAYMENT_API_KEY_SECONDARY')]);
         }
     }
