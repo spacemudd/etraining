9116f8cb95d47ee55359255f4cba7d81f125b409
Samar Alali <alalisummer@gmail.com>
2023-06-07 10:42:42 +0300

    Certificate (اساسيات الاعمال المكتبية)

--- backend/resources/views/pdf/certificate/show.blade.php
+++ backend/resources/views/pdf/certificate/show.blade.php
@@ -55,11 +55,11 @@
         <h1 style="margin-top:8rem;">شهادة حضور دورة تطويرية</h1>
         <p style="margin-top:2.5rem;">يشهد مركز احترافية المدرب للتدريب بأن المتدربة  "{{ $certificate->trainee->name }}"</p>
         <p style="margin-top:2.5rem;">وجنسيتها سعودية بموجب السجل المدني رقم "{{ $certificate->trainee->identity_number }}"</p>
-        <p style="margin-top:2.5rem;">قـــد حضرت دورة ({{ $certificate->course->name_ar }}) عدد ساعاتها التدريبية 32 ساعة</p>
-        <p style="margin-top:2.5rem;">وعدد أيامها التدريبية (30) أيام والمنعقدة في الفترة من </p>
-        <p style="margin-top:2.5rem;">(7 -10 - 1443هـ  إلى 6 - 4 - 1444هـ  )</p>
+        <p style="margin-top:2.5rem;">قـــد حضرت دورة ({{ $certificate->course->name_ar }}) عدد ساعاتها التدريبية 16 ساعة</p>
+        <p style="margin-top:2.5rem;">وعدد أيامها التدريبية (8) أيام والمنعقدة في الفترة من </p>
+        <p style="margin-top:2.5rem;">(26 - 2 - 1443هـ  إلى 21 - 3 - 1443هـ  )</p>
         <p style="margin-top:2.5rem;">والمعتمدة من المؤسسة العامة للتدريب التقني والمهني برقم </p>
-        <p style="margin-top:2.5rem;">(192246234)</p>
+        <p style="margin-top:2.5rem;">(192285433)</p>
         <p style="margin-top:5rem;">والله الموفق،،،</p>
         <div class="row" style="margin-top: 2rem;">
             <div class="col-7">
@@ -68,8 +68,8 @@
             </div>
             <div class="col-4" style="text-align: right;">
                 <p class="small-text">
-                    <b>حررت بتاريخ:    6 / 4  /1444  هـ </b><br/>
-                    <b>الموافق:            31 /  10 /2022 م</b>
+                    <b>حررت بتاريخ:    21 / 3  /1443  هـ </b><br/>
+                    <b>الموافق:            27 /  10 /2021 م</b>
                 </p>
                 <p class="small-text">ملاحظة: أي كشط او تعديل على الشهادة تعتبر لاغية</p>
             </div>
