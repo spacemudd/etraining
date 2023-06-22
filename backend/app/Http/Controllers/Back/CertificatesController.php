ee2b956dc38050163ae7c0009a05eb77e0586cf2
Samar Alali <alalisummer@gmail.com>
2023-06-22 10:56:44 +0300

    Certificate (خدمة عملاء (الجديدة))

--- backend/app/Http/Controllers/Back/CertificatesController.php
+++ backend/app/Http/Controllers/Back/CertificatesController.php
@@ -27,7 +27,7 @@ class CertificatesController extends Controller
         $filepath = storage_path('app').'/'.$path;
 
         $import = new CertificatesImport();
-        $import->course_id = Course::find('f0f6bed6-10fe-4f22-9abf-6421159954cb')->id ?? Course::first()->id;
+        $import->course_id = Course::find('c55b4647-8267-4420-a73d-394d5d71c310')->id ?? Course::first()->id;
         $import->status = CertificatesImport::STATUS_IMPORTING;
         $import->processed_count = 0;
         $import->total_count = 0;
