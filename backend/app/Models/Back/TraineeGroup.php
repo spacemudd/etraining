36374b20353fceb6927ae7404a0648f7a17446d6
shafiqalshaar <shafiqalshaar@gmail.com>
2023-07-23 13:59:48 +0300

    Fix timing for group 1

--- backend/app/Models/Back/TraineeGroup.php
+++ backend/app/Models/Back/TraineeGroup.php
@@ -70,7 +70,7 @@ class TraineeGroup extends Model implements Auditable
     {
         switch ($this->name) {
             case 'شعبة 1':
-                return 'كل أحد والإثنين الساعة 10:00 الى 12:00 ظهراً';
+                return 'كل أحد والإثنين الساعة 01:00 الى 03:00 مساءً';
             case 'شعبة 2':
                 return 'كل ثلاثاء والأربعاء الساعة 10:00 الى 12:00 ظهراً';
             case 'شعبة 3':
