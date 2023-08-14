564a8e082f60dc6d90476d07828d72a46504012c
Samar Alali <alalisummer@gmail.com>
2023-08-14 11:05:30 +0300

    WIP

--- backend/resources/views/pdf/certificate/show.blade.php
+++ backend/resources/views/pdf/certificate/show.blade.php
@@ -63,8 +63,8 @@
         <p style="margin-top:5rem;">والله الموفق،،،</p>
         <div class="row" style="margin-top: 2rem;">
             <div class="col-7">
-                <p style="margin-bottom:0;padding-bottom: 0"><b>ختم المنشأة التدريبية</b></p><br>
-                <img id="page-stamp" src="{{ public_path('/img/certificate-stamp-new.png') }}">
+                <p style="margin-bottom:0;padding-bottom: 0"><b>ختم المنشأة التدريبية</b></p>
+                <img id="page-stamp" src="{{ public_path('/img/certificate-stamp-new-resize.png') }}">
             </div>
             <div class="col-4" style="text-align: right;">
                 <p class="small-text">
