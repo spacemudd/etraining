062261c90c9882208814bddf0d592bf64ec1a56c
Samar Alali <alalisummer@gmail.com>
2023-08-14 10:56:17 +0300

    Add new Certificate Stamp

--- backend/resources/views/pdf/certificate/show.blade.php
+++ backend/resources/views/pdf/certificate/show.blade.php
@@ -64,7 +64,7 @@
         <div class="row" style="margin-top: 2rem;">
             <div class="col-7">
                 <p style="margin-bottom:0;padding-bottom: 0"><b>ختم المنشأة التدريبية</b></p>
-                <img id="page-stamp" src="{{ public_path('/img/certificate-stamp.png') }}">
+                <img id="page-stamp" src="{{ public_path('/img/certificate-stamp-new.png') }}">
             </div>
             <div class="col-4" style="text-align: right;">
                 <p class="small-text">
