75b141f2cf1504fe3c1365f63b21019a1a95620a
Samar Alali <alalisummer@gmail.com>
2023-08-16 10:09:31 +0300

    Final Design

--- backend/resources/views/pdf/certificate/show.blade.php
+++ backend/resources/views/pdf/certificate/show.blade.php
@@ -61,23 +61,24 @@
         <img class="page-logo" src="{{ public_path('/img/logo-lg.png') }}">
 
         <div class="row" style="margin-top: 2rem;">
-            <h1>شهادة حضور دورة تطويرية</h1>
+            <h1 style="margin-top:8rem;">شهادة حضور دورة تطويرية</h1>
             <p style="margin-top:2.5rem;">يشهد مركز احترافية المدرب للتدريب بأن المتدربة  "{{ $certificate->trainee->name }}"</p>
             <p style="margin-top:2.5rem;">وجنسيتها سعودية بموجب السجل المدني رقم "{{ $certificate->trainee->identity_number }}"</p>
-            <p style="margin-top:2.5rem;">قـــد حضرت دورة ({{ $certificate->course->name_ar }}) عدد ساعاتها التدريبية 30 ساعة</p>
-            <p style="margin-top:2.5rem;">وعدد أيامها التدريبية (5) أيام والمنعقدة في الفترة من </p>
-            <p style="margin-top:2.5rem;">(13 - 8 - 1444هـ  إلى 7 - 9 - 1444هـ  )</p>
+            <p style="margin-top:2.5rem;">قـــد حضرت دورة ({{ $certificate->course->name_ar }}) عدد ساعاتها التدريبية 10 ساعة</p>
+            <p style="margin-top:2.5rem;">وعدد أيامها التدريبية (2) أيام والمنعقدة في الفترة من </p>
+            <p style="margin-top:2.5rem;">(19 - 12 - 1444هـ  إلى 15 - 01 - 1445هـ  )</p>
             <p style="margin-top:2.5rem;">والمعتمدة من المؤسسة العامة للتدريب التقني والمهني برقم </p>
-            <p style="margin-top:2.5rem;">(192257914)</p>
+            <p style="margin-top:2.5rem;">(19222918)</p>
             <p style="margin-top:5rem;">والله الموفق،،،</p>
             <div class="col-7">
                 <p style="margin-bottom:0;padding-bottom: 0"><b>ختم المنشأة التدريبية</b></p>
-                <img id="page-stamp" src="{{ public_path('/img/certificate-stamp.png') }}">
+                <img style="padding-top: 20px" id="page-stamp" src="{{ public_path('/img/certificate-stamp-new.png') }}">
             </div>
             <div class="col-4" style="text-align: right;">
+                <p>توقيع المديرة: <img  id="page-stamp" src="{{ public_path('/img/signature.png') }}"></p>
                 <p class="small-text">
-                    <b>حررت بتاريخ:    7 / 9  /1444  هـ </b><br/>
-                    <b>الموافق:            29 /  3 /2023 م</b>
+                    <b>حررت بتاريخ:    16 / 1  /1445  هـ </b><br/>
+                    <b>الموافق:            3 /  8 /2023 م</b>
                 </p>
                 <p class="small-text">ملاحظة: أي كشط او تعديل على الشهادة تعتبر لاغية</p>
             </div>
