1b8fb9845ccb3e653173d898bda56b12a32259c2
shafiqalshaar <shafiqalshaar@gmail.com>
2023-08-27 15:38:07 +0300

    Company attendance report 2023-08

--- backend/app/Console/Commands/AutomateCompanyAttendanceSheetsCommand.php
+++ backend/app/Console/Commands/AutomateCompanyAttendanceSheetsCommand.php
@@ -53,7 +53,7 @@ class AutomateCompanyAttendanceSheetsCommand extends Command
 
             // Has report for current month?
             $currentMonthReport = $company->company_attendance_reports()
-                ->whereBetween('date_from', ['2023-07-01', '2023-07-31'])
+                ->whereBetween('date_from', ['2023-08-01', '2023-08-31'])
                 ->first();
 
             if ($currentMonthReport) {
@@ -67,23 +67,23 @@ class AutomateCompanyAttendanceSheetsCommand extends Command
 
             if ($lastReport && $lastReport->to_emails) {
                 // Is the number of trainees equal to the number of trainees in the company?
-                //if ($lastReport->trainees()->count() !== $company->trainees()->count()) {
-                //    $this->info('Number of trainees in the last report is not equal to the number of trainees in the company. Skipping: '.$company->name_ar);
-                //    continue;
-                //}
+                if ($lastReport->trainees()->count() !== $company->trainees()->count()) {
+                    $this->info('Number of trainees in the last report is not equal to the number of trainees in the company. Skipping: '.$company->name_ar);
+                    continue;
+                }
 
                 // Are the trainees matching the IDs of the all the trainees in the company?
-                //foreach ($lastReport->trainees as $trainee) {
-                //    if (! $company->trainees()->where('id', $trainee->id)->first()) {
-                //        $this->info('Trainee not found in the company. Skipping: '.$company->name_ar);
-                //        continue 2;
-                //    }
-                //}
+                foreach ($lastReport->trainees as $trainee) {
+                    if (! $company->trainees()->where('id', $trainee->id)->first()) {
+                        $this->info('Trainee not found in the company. Skipping: '.$company->name_ar);
+                        continue 2;
+                    }
+                }
 
                 //$clone = app()->make(CompanyAttendanceReportService::class)->clone($lastReport->id);
                 $clone = app()->make(CompanyAttendanceReportService::class)->newReport($company->id);
-                $clone->date_from = Carbon::parse('2023-07-01')->setTimezone('Asia/Riyadh')->startOfDay();
-                $clone->date_to = Carbon::parse('2023-07-31')->setTimezone('Asia/Riyadh')->endOfDay();
+                $clone->date_from = Carbon::parse('2023-08-01')->setTimezone('Asia/Riyadh')->startOfDay();
+                $clone->date_to = Carbon::parse('2023-08-31')->setTimezone('Asia/Riyadh')->endOfDay();
                 $clone->cc_emails = Str::replace('ptc-ksa.com', 'ptc-ksa.net', $lastReport->cc_emails);
                 if ($company->salesperson_email && !Str::contains($clone->cc_emails, $company->salesperson_email)) {
                     $clone->cc_emails .= ', '.$company->salesperson_email;
@@ -97,8 +97,8 @@ class AutomateCompanyAttendanceSheetsCommand extends Command
                 }
                 $this->info('No last report. Creating new report - '.$company->name_ar);
                 $report = app()->make(CompanyAttendanceReportService::class)->newReport($company->id);
-                $report->date_from = Carbon::parse('2023-07-01')->setTimezone('Asia/Riyadh')->startOfDay();
-                $report->date_to = Carbon::parse('2023-07-31')->setTimezone('Asia/Riyadh')->endOfDay();
+                $report->date_from = Carbon::parse('2023-08-01')->setTimezone('Asia/Riyadh')->startOfDay();
+                $report->date_to = Carbon::parse('2023-08-31')->setTimezone('Asia/Riyadh')->endOfDay();
                 $report->cc_emails = 'sara@ptc-ksa.net, m_shehatah@ptc-ksa.net, ceo@ptc-ksa.net, mashael.a@ptc-ksa.net';
                 if ($company->salesperson_email && !Str::contains($report->cc_emails, $company->salesperson_email)) {
                     $report->cc_emails .= ', '.$company->salesperson_email;
