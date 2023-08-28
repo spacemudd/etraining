bcd627ce868440fa5b7fdb1b84b1d14ad4e3b2d7
Samar Alali <alalisummer@gmail.com>
2023-08-28 15:43:54 +0300

    Edit Survey

--- backend/resources/js/Pages/Survey/Index.vue
+++ backend/resources/js/Pages/Survey/Index.vue
@@ -1,12 +1,15 @@
 <template>
     <app-layout>
         <div class="container px-6 mx-auto grid">
+            <div class="flex content-center mt-20 text-center w-full" v-if="">
+                <a target="_blank" rel="noopener noreferrer" href="https://forms.gle/wZtJ4n3kLRv3AVfH7" class="bg-red-500 px-2 py-1 rounded mx-auto text-white hover:bg-red-700">قياس رضا المتدرب</a>
+            </div>
             <div class="flex content-center mt-20 text-center w-full">
-                <a target="_blank" rel="noopener noreferrer" href="https://forms.gle/ikw2m17MUcE5js8K7" class="bg-red-500 px-2 py-1 rounded mx-auto text-white hover:bg-red-700">اضغط لبدأ الإستبيان</a>
+                <a target="_blank" rel="noopener noreferrer" href="https://forms.gle/EqVJ6e29NHfQbQDY7" class="bg-red-500 px-2 py-1 rounded mx-auto text-white hover:bg-red-700">قياس رضا المدرب</a>
             </div>
 
             <div class="mt-20 bg-white shadow-lg rounded-lg p-5 text-center flex content-center">
-                <img src="/img/ptc_survey.png" class="mx-auto" style="max-width:600px;">
+                <img src="/img/ptc_survey_new.png" class="mx-auto" style="max-width:600px;">
             </div>
         </div>
     </app-layout>
