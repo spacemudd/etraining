767905365d0e1c3e87d62c8af6b8f165c0f9a702
shafiqalshaar <shafiqalshaar@gmail.com>
2023-10-03 17:38:02 +0300

    Small footer size

--- backend/resources/views/vendor/mail/html/footer.blade.php
+++ backend/resources/views/vendor/mail/html/footer.blade.php
@@ -3,12 +3,12 @@
 <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
 <tr>
 <td class="content-cell" align="center">
-    البريد الالكتروني مرسل عن طريق النظام الالكتروني بشكل تلقائي في حالة وجود اي ملاحظة لا تترددوا بالتواصل معنا على البريد الالكتروني المعتمد
+{{ Illuminate\Mail\Markdown::parse($slot) }}
 </td>
 </tr>
-<tr>
-<td class="content-cell" align="center">
-{{ Illuminate\Mail\Markdown::parse($slot) }}
+    <tr>
+<td class="content-cell text-xs" align="center">
+    البريد الالكتروني مرسل عن طريق النظام الالكتروني بشكل تلقائي في حالة وجود اي ملاحظة لا تترددوا بالتواصل معنا على البريد الالكتروني المعتمد
 </td>
 </tr>
 </table>
