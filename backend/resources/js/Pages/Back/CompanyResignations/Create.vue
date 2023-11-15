f49ab69640f5fd918da4d3a79ccadd9016f1eb2d
shafiqalshaar <shafiqalshaar@gmail.com>
2023-11-15 16:43:38 +0300

    Add new reason

--- backend/resources/js/Pages/Back/CompanyResignations/Create.vue
+++ backend/resources/js/Pages/Back/CompanyResignations/Create.vue
@@ -50,6 +50,7 @@
                                                     <option value="انسحاب">انسحاب</option>
                                                     <option value="عمل مشاكل">عمل مشاكل</option>
                                                     <option value="غير مسجلة في الشركة">غير مسجلة في الشركة</option>
+                                                    <option value="عدم الالتزام في شروط المعهد">عدم الالتزام في شروط المعهد</option>
                                                 </select>
                                                 <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                                                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
