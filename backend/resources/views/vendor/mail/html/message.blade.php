be3d000d03579fefb7a7b7fd1d28f127760ace42
shafiqalshaar <shafiqalshaar@gmail.com>
2023-11-19 11:50:15 +0300

    Change logo in emails sent

--- backend/resources/views/vendor/mail/html/message.blade.php
+++ backend/resources/views/vendor/mail/html/message.blade.php
@@ -3,7 +3,7 @@
 @slot('header')
 @component('mail::header', ['url' => config('app.url')])
 <a href="https://app.ptc-ksa.net" style="display: inline-block;">
-    <img src="{{ asset('/img/logo.png') }}" style="width:150px;" alt="PTC Logo">
+    <img src="{{ asset('/img/logox.png') }}" style="width:150px;" alt="شركة مركز احترافية التدريب">
 </a>
 @endcomponent
 @endslot
