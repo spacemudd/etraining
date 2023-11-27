508062698c48f2f2bdbb4aa6daa735b47a6378bb
shafiqalshaar <shafiqalshaar@gmail.com>
2023-11-27 11:01:01 +0300

    Update attendance sheet

--- backend/app/Console/Commands/AutomateCompanyAttendanceSheetsCommand.php
+++ backend/app/Console/Commands/AutomateCompanyAttendanceSheetsCommand.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Back\Company;
+use App\Models\Back\Invoice;
 use App\Services\CompanyAttendanceReportService;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
@@ -53,7 +54,7 @@ class AutomateCompanyAttendanceSheetsCommand extends Command
 
             // TODO: Has report for current month? Update for the current month
             $currentMonthReport = $company->company_attendance_reports()
-                ->whereBetween('date_from', ['2023-08-01', '2023-08-31'])
+                ->whereBetween('date_to', ['2023-11-01', '2023-11-30'])
                 ->first();
 
             if ($currentMonthReport) {
@@ -80,32 +81,38 @@ class AutomateCompanyAttendanceSheetsCommand extends Command
                     }
                 }
 
-                // TODO: Choose, clone from previous period or new report with current months' students?
-                //$clone = app()->make(CompanyAttendanceReportService::class)->clone($lastReport->id);
-                $clone = app()->make(CompanyAttendanceReportService::class)->newReport($company->id);
-                $clone->date_from = Carbon::parse('2023-08-01')->setTimezone('Asia/Riyadh')->startOfDay();
-                $clone->date_to = Carbon::parse('2023-08-31')->setTimezone('Asia/Riyadh')->endOfDay();
-                $clone->cc_emails = Str::replace('ptc-ksa.com', 'ptc-ksa.net', $lastReport->cc_emails);
-                if ($company->salesperson_email && !Str::contains($clone->cc_emails, $company->salesperson_email)) {
-                    $clone->cc_emails .= ', '.$company->salesperson_email;
+                if (Invoice::where('company_id', $company->id)->whereBetween('to_date', ['2023-10-01', '2023-11-30'])->count() === 0) {
+                    $this->info('No invoices in 2023-10 -> 2023-11. Skipping: '.$company->name_ar);
+                    continue;
                 }
-                $clone->save();
-                app()->make(CompanyAttendanceReportService::class)->approve($clone->id);
+
+                // TODO: Choose, clone from previous period or new report with current months' students?
+                ////$clone = app()->make(CompanyAttendanceReportService::class)->clone($lastReport->id);
+                //$clone = app()->make(CompanyAttendanceReportService::class)->newReport($company->id);
+                //
+                //$clone->date_from = Carbon::parse('2023-08-01')->setTimezone('Asia/Riyadh')->startOfDay();
+                //$clone->date_to = Carbon::parse('2023-08-31')->setTimezone('Asia/Riyadh')->endOfDay();
+                //$clone->cc_emails = Str::replace('ptc-ksa.com', 'ptc-ksa.net', $lastReport->cc_emails);
+                //if ($company->salesperson_email && !Str::contains($clone->cc_emails, $company->salesperson_email)) {
+                //    $clone->cc_emails .= ', '.$company->salesperson_email;
+                //}
+                //$clone->save();
+                //app()->make(CompanyAttendanceReportService::class)->approve($clone->id);
             } else {
                 if (!$company->email) {
                     $this->info('No email for company. Skipping: '.$company->name_ar);
                     continue;
                 }
                 $this->info('No last report. Creating new report - '.$company->name_ar);
-                $report = app()->make(CompanyAttendanceReportService::class)->newReport($company->id);
-                $report->date_from = Carbon::parse('2023-08-01')->setTimezone('Asia/Riyadh')->startOfDay();
-                $report->date_to = Carbon::parse('2023-08-31')->setTimezone('Asia/Riyadh')->endOfDay();
-                $report->cc_emails = 'sara@ptc-ksa.net, m_shehatah@ptc-ksa.net, ceo@ptc-ksa.net, mashael.a@ptc-ksa.net';
-                if ($company->salesperson_email && !Str::contains($report->cc_emails, $company->salesperson_email)) {
-                    $report->cc_emails .= ', '.$company->salesperson_email;
-                }
-                $report->save();
-                app()->make(CompanyAttendanceReportService::class)->approve($report->id);
+                //$report = app()->make(CompanyAttendanceReportService::class)->newReport($company->id);
+                //$report->date_from = Carbon::parse('2023-08-01')->setTimezone('Asia/Riyadh')->startOfDay();
+                //$report->date_to = Carbon::parse('2023-08-31')->setTimezone('Asia/Riyadh')->endOfDay();
+                //$report->cc_emails = 'sara@ptc-ksa.net, m_shehatah@ptc-ksa.net, ceo@ptc-ksa.net, mashael.a@ptc-ksa.net';
+                //if ($company->salesperson_email && !Str::contains($report->cc_emails, $company->salesperson_email)) {
+                //    $report->cc_emails .= ', '.$company->salesperson_email;
+                //}
+                //$report->save();
+                //app()->make(CompanyAttendanceReportService::class)->approve($report->id);
             }
         }
 
