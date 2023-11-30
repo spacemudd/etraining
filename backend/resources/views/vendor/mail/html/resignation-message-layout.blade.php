cd6fe1b5b756dad4e596781c3c28738603f61ef8
shafiqalshaar <shafiqalshaar@gmail.com>
2023-11-30 16:50:02 +0300

    Change message type

---
+++ backend/resources/views/vendor/mail/html/resignation-message-layout.blade.php
@@ -0,0 +1,29 @@
+@component('mail::layout')
+{{-- Header --}}
+@slot('header')
+@component('mail::header', ['url' => config('app.url')])
+<a href="https://app.ptc-ksa.net" style="display: inline-block;">
+    <img src="{{ asset('/img/logox.png') }}" style="width:150px;" alt="شركة مركز احترافية التدريب">
+</a>
+@endcomponent
+@endslot
+
+{{-- Body --}}
+{{ $slot }}
+
+{{-- Subcopy --}}
+@isset($subcopy)
+@slot('subcopy')
+@component('mail::subcopy')
+{{ $subcopy }}
+@endcomponent
+@endslot
+@endisset
+
+{{-- Footer --}}
+@slot('footer')
+@component('mail::footer')
+© {{ date('Y') }} {{ config('app.developer-name') }}. @lang('words.all-rights-reserved')
+@endcomponent
+@endslot
+@endcomponent
