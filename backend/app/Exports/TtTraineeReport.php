09194469f5ac3fa66ec79d991289d967fe8203c7
shafiqalshaar <shafiqalshaar@gmail.com>
2023-12-12 18:58:43 +0300

    wip

--- backend/app/Exports/TtTraineeReport.php
+++ backend/app/Exports/TtTraineeReport.php
@@ -48,8 +48,8 @@ class TtTraineeReport implements FromArray
                 'email' => $trainee->email,
                 'phone' => $trainee->clean_phone,
                 'absences_custom' => $trainee->absences_custom_count ?: 0,
-                'oldest_absence' => $trainee->absences_custom()->oldest()->first(),
-                'latest_absence' => $trainee->absences_custom()->latest()->first(),
+                'oldest_absence' => $trainee->absences_custom()->oldest()->first()->created_at,
+                'latest_absence' => $trainee->absences_custom()->latest()->first()->created_at,
             ];
         }
 
