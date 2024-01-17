887b594d42e805c51def01a58ea3243d6f770b4c
Samar Alali <alalisummer@gmail.com>
2024-01-17 10:04:11 +0300

    Add fields to (Suspend - Block)

--- backend/resources/js/Pages/Back/Trainees/Block.vue
+++ backend/resources/js/Pages/Back/Trainees/Block.vue
@@ -24,6 +24,8 @@
                             <option value="عمل مشاكل">عمل مشاكل</option>
                             <option value="غير مسجلة في الشركة">غير مسجلة في الشركة</option>
                             <option value="عدم الالتزام في شروط المعهد">عدم الالتزام في شروط المعهد</option>
+                            <option value="عدم الالتزام في شروط المعهد">رجيع</option>
+                            <option value="عدم الالتزام في شروط المعهد">غير مؤهل</option>
                         </select>
                         <button type="submit" class="mt-5 items-center justify-start float-left rounded-md px-4 py-2 bg-red-300 hover:bg-red-400">
                         {{ $t('words.block-trainee') }}
