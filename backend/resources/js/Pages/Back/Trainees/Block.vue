8b1a29e74126edb95c220446624283002372084a
shafiqalshaar <shafiqalshaar@gmail.com>
2024-01-30 16:15:01 +0300

    Add new reasons

--- backend/resources/js/Pages/Back/Trainees/Block.vue
+++ backend/resources/js/Pages/Back/Trainees/Block.vue
@@ -27,6 +27,7 @@
                             <option value="رجيع">رجيع</option>
                             <option value="غير مؤهل">غير مؤهل</option>
                             <option value="حساب مكرر">حساب مكرر</option>
+                            <option value="استبعاد لعدم الحضور">استبعاد لعدم الحضور</option>
                         </select>
                         <button type="submit" class="mt-5 items-center justify-start float-left rounded-md px-4 py-2 bg-red-300 hover:bg-red-400">
                             {{ $t('words.block-trainee') }}
