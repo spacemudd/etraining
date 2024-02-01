87bfaa38cf3261900cff9e8565ec17c2a2bbe35f
shafiqalshaar <shafiqalshaar@gmail.com>
2024-02-01 11:34:47 +0300

    Fix email formatting

--- backend/resources/views/emails/new-company.blade.php
+++ backend/resources/views/emails/new-company.blade.php
@@ -5,7 +5,7 @@
 </head>
 <body style="text-align:right;">
 الاسم: {{ $company_name }}<br/>
-الرابط: <a href="{{ $company_link }}">{{ $company_link }}</a>
+الرابط: <br/><a href="{{ $company_link }}">{{ $company_link }}</a>
 <br/>
 ---<br/>
 <span style="font-size:8px">هذا البريد الإلكتروني تم إرساله تلقائيا من المنصة <a href="https://app.ptc-ksa.net">app.ptc-ksa.net</a></span>
