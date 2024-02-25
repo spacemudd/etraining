d239c6613b70edf752da5a0c6418652cbf4317b8
Samar Alali <alalisummer@gmail.com>
2024-02-25 10:53:31 +0300

    Certificate (المسؤول الإداري الفعال (Without Dates))

--- backend/app/Http/Controllers/Back/CertificatesController.php
+++ backend/app/Http/Controllers/Back/CertificatesController.php
@@ -27,7 +27,7 @@ class CertificatesController extends Controller
         $filepath = storage_path('app').'/'.$path;
 
         $import = new CertificatesImport();
-        $import->course_id = Course::find('9a874848-e9af-4166-9ddd-c62382d7fb18')->id ?? Course::first()->id;
+        $import->course_id = Course::find('6b7a7468-aff1-4c64-a5d1-469779835711')->id ?? Course::first()->id;
         $import->status = CertificatesImport::STATUS_IMPORTING;
         $import->processed_count = 0;
         $import->total_count = 0;
