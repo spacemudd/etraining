4b757e6b50c3c2e317a0d49fcf0e84404fb82972
Samar Alali <alalisummer@gmail.com>
2024-02-25 10:26:15 +0300

    Certificate (اعداد وكتابة التقارير (Without Dates))

--- backend/app/Http/Controllers/Back/CertificatesController.php
+++ backend/app/Http/Controllers/Back/CertificatesController.php
@@ -27,7 +27,7 @@ class CertificatesController extends Controller
         $filepath = storage_path('app').'/'.$path;
 
         $import = new CertificatesImport();
-        $import->course_id = Course::find('f2767d89-d48b-4d18-9203-d274bef6d406')->id ?? Course::first()->id;
+        $import->course_id = Course::find('3e75b680-17af-4767-96e4-0c6e9eacedd3')->id ?? Course::first()->id;
         $import->status = CertificatesImport::STATUS_IMPORTING;
         $import->processed_count = 0;
         $import->total_count = 0;
