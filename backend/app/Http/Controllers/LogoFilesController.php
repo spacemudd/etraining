d785252eb3a1b02a99f5b2e05e5132250ba0a76c
test <ebrahim-hosny@ptc-ksa.net>
2024-07-01 16:28:17 +0300

    control logo

--- backend/app/Http/Controllers/LogoFilesController.php
+++ backend/app/Http/Controllers/LogoFilesController.php
@@ -12,7 +12,7 @@ class LogoFilesController extends Controller
     public function index(){
 
         return Inertia::render('Back/Companies/CompaniesLogo/Index', [
-            'companies' => Company::with('logo_files')->paginate(500),
+            'companies' => Company::with('logo_files')->paginate(300),
         ]);
     }
 
