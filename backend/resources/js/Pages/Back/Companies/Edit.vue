fbd52c04bb7ad02edc734af69e8211d28bdd5991
test <ebrahim-hosny@ptc-ksa.net>
2024-07-11 10:07:47 +0300

    hhh

--- backend/resources/js/Pages/Back/Companies/Edit.vue
+++ backend/resources/js/Pages/Back/Companies/Edit.vue
@@ -63,7 +63,6 @@
                   class="block appearance-none w-full border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                   v-model="form.recruitment_company_id"
                   id="educational_level_id"
-                  required
                 >
                   <option value=""></option>
                   <option
