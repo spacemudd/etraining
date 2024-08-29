0e2ddf82d9e20f54cc25175b60bbb1e3bb89375e
shafiqalshaar <shafiqalshaar@gmail.com>
2024-08-29 12:43:50 +0300

    Fix attendance report

--- backend/resources/js/Components/CourseBatchSessionsList.vue
+++ backend/resources/js/Components/CourseBatchSessionsList.vue
@@ -28,39 +28,41 @@
                 <span class="text-xs">{{ session.ends_at_timezone | timestampDate }}</span>
             </td>
             <td class="border-t py-3 text-left flex flex-col">
-                <a v-if="can('download-attendance-sheet-for-course-batch') && session.attendance_report.can_prepare_attendance"
-                              :href="route('teaching.attendance-reports.excel', {attendance_report_id: session.attendance_report.id})"
-                   target="_blank"
-                              class="bg-gray-200 py-1 px-2 rounded text-black text-sm hover:bg-gray-300 w-full text-center">
-                    <img src="/img/excel.svg" class="float inline-block ml-2" style="max-width:16px;">
-                    {{ $t('words.attendance') }}
-                </a>
+                <template v-if="session.attendance_report">
+                    <a v-if="can('download-attendance-sheet-for-course-batch') && session.attendance_report.can_prepare_attendance"
+                                  :href="route('teaching.attendance-reports.excel', {attendance_report_id: session.attendance_report.id})"
+                       target="_blank"
+                                  class="bg-gray-200 py-1 px-2 rounded text-black text-sm hover:bg-gray-300 w-full text-center">
+                        <img src="/img/excel.svg" class="float inline-block ml-2" style="max-width:16px;">
+                        {{ $t('words.attendance') }}
+                    </a>
 
-                <template v-if="session.attendance_report.can_prepare_attendance">
-                    <template v-if="session.attendance_report.submitted_by">
-                        <p class="bg-green-100 py-1 px-2 rounded text-black text-sm mt-5 w-full text-center opacity-75 border-2 border-green-300">
-                            {{ $t('words.attendance-submitted') }} ✔
+                    <template v-if="session.attendance_report.can_prepare_attendance">
+                        <template v-if="session.attendance_report.submitted_by">
+                            <p class="bg-green-100 py-1 px-2 rounded text-black text-sm mt-5 w-full text-center opacity-75 border-2 border-green-300">
+                                {{ $t('words.attendance-submitted') }} ✔
+                            </p>
+                        </template>
+                        <inertia-link v-else :href="route('teaching.course-batch-sessions.attendance-reports.show', {course_batch_session_id: session.id})"
+                                      class="bg-blue-600 py-1 px-2 rounded text-white text-sm hover:bg-blue-800 mt-5 w-full text-center">
+                            {{ $t('words.prepare-attendance') }}
+                        </inertia-link>
+                    </template>
+                    <template v-else>
+                        <p class="bg-gray-500 py-1 px-2 rounded text-white text-sm w-full text-center opacity-75">
+                            {{ $t('words.please-wait-until-session-is-over') }}
                         </p>
                     </template>
-                    <inertia-link v-else :href="route('teaching.course-batch-sessions.attendance-reports.show', {course_batch_session_id: session.id})"
-                                  class="bg-blue-600 py-1 px-2 rounded text-white text-sm hover:bg-blue-800 mt-5 w-full text-center">
-                        {{ $t('words.prepare-attendance') }}
-                    </inertia-link>
-                </template>
-                <template v-else>
-                    <p class="bg-gray-500 py-1 px-2 rounded text-white text-sm w-full text-center opacity-75">
-                        {{ $t('words.please-wait-until-session-is-over') }}
-                    </p>
-                </template>
 
-                <button class="bg-red-600 py-1 px-2 rounded text-white text-sm hover:bg-red-800 mt-5 w-full disabled:bg-gray-500"
-                        v-if="session.can_be_deleted"
-                        :class="{'btn-disabled': $wait.is('DELETING_SESSION_'+session.id)}"
-                        :id="session.id"
-                        :disabled="$wait.is('DELETING_SESSION_'+session.id)"
-                        @click.prevent="deleteSession(session)">
-                    {{ $t('words.cancel') }}
-                </button>
+                    <button class="bg-red-600 py-1 px-2 rounded text-white text-sm hover:bg-red-800 mt-5 w-full disabled:bg-gray-500"
+                            v-if="session.can_be_deleted"
+                            :class="{'btn-disabled': $wait.is('DELETING_SESSION_'+session.id)}"
+                            :id="session.id"
+                            :disabled="$wait.is('DELETING_SESSION_'+session.id)"
+                            @click.prevent="deleteSession(session)">
+                        {{ $t('words.cancel') }}
+                    </button>
+                </template>
 
             </td>
         </tr>
