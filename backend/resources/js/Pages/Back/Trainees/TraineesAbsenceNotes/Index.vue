edbc08462f0e33aa83c2070b110ce777b629a401
test <ebrahim-hosny@ptc-ksa.net>
2024-09-03 13:45:17 +0300

    check

--- backend/resources/js/Pages/Back/Trainees/TraineesAbsenceNotes/Index.vue
+++ backend/resources/js/Pages/Back/Trainees/TraineesAbsenceNotes/Index.vue
@@ -91,6 +91,7 @@
                                         <div v-if="note.rejected_at">
                                             {{ $t('words.rejected') }}
                                             <span class="text-xs" dir="ltr">{{ note.rejected_at_timezone }}</span>
+                                             <h1>here</h1>
                                              <div v-if="note.rejected_reason" class="mt-2">
                                                   <h1>hiii</h1>
                                                  <strong>{{ $t('words.reject-reason') }}:</strong> {{ note.rejected_reason }}
