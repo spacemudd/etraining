b893b4523612f2cea922a56e152bda90162e4697
shafiqalshaar <shafiqalshaar@gmail.com>
2024-09-19 17:05:19 +0300

    Fix model import

--- backend/app/Models/Back/Company.php
+++ backend/app/Models/Back/Company.php
@@ -2,14 +2,8 @@
 
 namespace App\Models\Back;
 
-use App\Models\Back\RecruitmentCompany;
-use App\Models\Back\Region;
-use App\Models\Back\RecruitmentCompany;
-use App\Models\Back\Region;
-use App\Models\CompanyAllowedUser;
 use App\Models\SearchableLabels;
 use App\Models\User;
-use Gate;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
