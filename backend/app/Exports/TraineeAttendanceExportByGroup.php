c041a056d6f1e09a52c653201f696f28b66dd888
test <ebrahim-hosny@ptc-ksa.net>
2024-09-25 14:58:46 +0300

    styles

---
+++ backend/app/Exports/TraineeAttendanceExportByGroup.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\Back\Trainee;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Illuminate\Contracts\Support\Responsable;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+
+class TraineeAttendanceExportByGroup implements FromCollection, WithHeadings, WithStyles, ShouldAutoSize
+{
+    protected $trainees;
+
+    public function __construct($trainees)
+    {
+        $this->trainees = $trainees;
+    }
+
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function collection()
+    {
+        return collect($this->trainees);
+    }
+
+    public function headings(): array
+    {
+        return ['Trainee Name', 'Present Count', 'Absent Count'];
+    }
+
+    /**
+     * Apply styles to the spreadsheet.
+     */
+    public function styles(Worksheet $sheet)
+    {
+        $sheet->getStyle('A1:C1')->getFont()->setBold(true);
+        
+        $sheet->getStyle('A1:C1')->getAlignment()->setHorizontal('center');
+
+        $sheet->getStyle('A1:C1')->getFill()
+            ->setFillType('solid')
+            ->getStartColor()->setARGB('FFFFE599'); 
+        
+        $sheet->getStyle('A:C')->getAlignment()->setHorizontal('center');
+    }
+}
