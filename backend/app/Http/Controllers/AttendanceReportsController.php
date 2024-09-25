3df91df201ef5ab4cdb0d5abec581830365bf6ca
test <ebrahim-hosny@ptc-ksa.net>
2024-09-25 17:08:09 +0300

    s

--- backend/app/Http/Controllers/AttendanceReportsController.php
+++ backend/app/Http/Controllers/AttendanceReportsController.php
@@ -184,9 +184,9 @@ class AttendanceReportsController extends Controller
                 $attendancePercentage = $totalSessionsCount > 0 ? ($presentCount / $totalSessionsCount) * 100 : 0;
     
                 $results[] = [
+                    'attendance_percentage' => round($attendancePercentage, 2) . ' %',
                     'present_count' => $presentCount,
                     'absent_count' => $absentCount,
-                    'attendance_percentage' => round($attendancePercentage, 2) . ' %',
                     'trainee_name' => $trainee->name,
                 ];
             }
