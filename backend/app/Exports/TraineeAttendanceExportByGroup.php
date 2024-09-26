828051980bd7bf3b10807ea71dd8bbcf8b1db5b4
test <ebrahim-hosny@ptc-ksa.net>
2024-09-26 10:15:11 +0300

    deserving certificates

--- backend/app/Exports/TraineeAttendanceExportByGroup.php
+++ backend/app/Exports/TraineeAttendanceExportByGroup.php
@@ -21,27 +21,46 @@ class TraineeAttendanceExportByGroup implements FromCollection, WithHeadings, Wi
 
     public function collection()
     {
-        return collect($this->trainees);
+        $collection = collect($this->trainees);
+    
+        foreach ($collection as $key => $trainee) {
+            $color = $trainee['certificate_color'];
+            $collection[$key]['certificate_eligibility'] = [
+                'value' => $trainee['certificate_eligibility'],
+                'color' => $color,
+            ];
+        }
+    
+        return $collection;
     }
 
     public function headings(): array
     {
-        return ['نسبة الحضور', 'عدد الحضور', 'عدد الغياب', 'اسم المتدرب'];
+        return ['استحقاق الشهادة','نسبة الحضور', 'عدد الحضور', 'عدد الغياب', 'اسم المتدرب'];
     }
 
     public function styles(Worksheet $sheet)
     {
-        $sheet->getStyle('A1:D1')->getFont()->setBold(true);
-        
-        $sheet->getStyle('A1:D1')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
-        
-        $sheet->getStyle('A1:D1')->getFill()
+        $sheet->getStyle('A1:E1')->getFont()->setBold(true);
+    
+        $sheet->getStyle('A1:E1')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
+    
+        $sheet->getStyle('A1:E1')->getFill()
             ->setFillType('solid')
             ->getStartColor()->setARGB('FFFFE599');
-
-        $sheet->getStyle('A:D')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
+    
+        $sheet->getStyle('A:E')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
+    
+        foreach ($this->trainees as $key => $trainee) {
+            $rowIndex = $key + 2; 
+            $color = $trainee['certificate_color'];
+            $sheet->getStyle("E$rowIndex")->getFont()->getColor()->setARGB($color); 
+        }
     }
 }
+
+
+
 
 
 
