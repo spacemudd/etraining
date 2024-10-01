22f5ad6b384466ec5366196cfea124e405bf13fe
Abdelrahman Yousef <abdelrahman.yousef@hadaf-hq.com>
2024-10-01 13:43:07 +0300

    up7

--- backend/config/noon_payment.php
+++ backend/config/noon_payment.php
@@ -5,21 +5,21 @@ return [
      */
     "register_routes" => true,
 
-    // Jisr Credentials
-    "business_id_jisr" => env('NOON_PAYMENT_BUSINESS_ID_JISR'),
-    "app_name_jisr" => env('NOON_PAYMENT_APP_NAME_JISR'),
-    "app_key_jisr" => env('NOON_PAYMENT_APP_KEY_JISR'),
-    "return_url_jisr" => env('NOON_PAYMENT_RETURN_URL_JISR'),
+    'business_id_jisarah' => env('NOON_PAYMENT_BUSINESS_ID_JASARAH'),
+    'app_name_jasarah' => env('NOON_PAYMENT_APP_NAME_JASARAH'),
+    'app_key_jasarah' => env('NOON_PAYMENT_APP_KEY_JASARAH'),
+    'return_url_jasarah' => env('NOON_PAYMENT_RETURN_URL_JASARAH'),
 
-    // Jasarah Credentials
-    "business_id_jasarah" => env('NOON_PAYMENT_BUSINESS_ID_JASARAH'),
-    "app_name_jasarah" => env('NOON_PAYMENT_APP_NAME_JASARAH'),
-    "app_key_jasarah" => env('NOON_PAYMENT_APP_KEY_JASARAH'),
-    "return_url_jasarah" => env('NOON_PAYMENT_RETURN_URL_JASARAH'),
+    'business_id_jisr' => env('NOON_PAYMENT_BUSINESS_ID_JISR'),
+    'app_name_jisr' => env('NOON_PAYMENT_APP_NAME_JISR'),
+    'app_key_jisr' => env('NOON_PAYMENT_APP_KEY_JISR'),
+    'return_url_jisr' => env('NOON_PAYMENT_RETURN_URL_JISR'),
 
-    "business_id" => env('NOON_PAYMENT_BUSINESS_ID'),
-    "app_name" => env('NOON_PAYMENT_APP_NAME'),
-    "app_key" => env('NOON_PAYMENT_APP_KEY'),
+    'mode' => env('NOON_PAYMENT_MODE', 'Test'),
+    'order_category' => env('NOON_PAYMENT_ORDER_CATEGORY', 'pay'),
+    'channel' => env('NOON_PAYMENT_CHANNEL', 'web'),
+    'payment_api' => env('NOON_PAYMENT_PAYMENT_API'),
+    'payment_api_test' => env('NOON_PAYMENT_PAYMENT_API_TEST'),
 
     /**
      *  Base64(BusinessIdentifier.ApplicationIdentifier:ApplicationKey)
