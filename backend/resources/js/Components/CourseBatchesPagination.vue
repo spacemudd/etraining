2ca0f8c5fa9481095e2dfb3017620f7057dc1739
test <ebrahim-hosny@ptc-ksa.net>
2024-10-01 12:05:35 +0300

    datails

--- backend/resources/js/Components/CourseBatchesPagination.vue
+++ backend/resources/js/Components/CourseBatchesPagination.vue
@@ -68,7 +68,7 @@
                                 @click="checkCertificateEligibility(batch.id)"
                                 class="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 mt-2"
                                 >
-                                استحقاق الشهادات
+                                استحقاق الشهادات بعد إنتهاء الدورة
                         </button>
                     </div>
                     <div class="col-span-1"></div>
