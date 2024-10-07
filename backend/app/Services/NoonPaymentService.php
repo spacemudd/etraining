ccabd4992e106b7765111bf11030b59e35f73185
test <ebrahim-hosny@ptc-ksa.net>
2024-10-07 16:13:56 +0300

    tt

--- backend/app/Services/NoonPaymentService.php
+++ backend/app/Services/NoonPaymentService.php
@@ -33,11 +33,11 @@ class NoonPaymentService
             $paymentInfo['order']['category'] = config("noon_payment.jisr.order_category");
             // Options for tokenize cc are (true - false)
             $paymentInfo['configuration']['tokenizeCc'] = (!empty($paymentInfo['configuration']['tokenizeCc'])) ? $paymentInfo['configuration']['tokenizeCc'] : "true";
-            $paymentInfo['configuration']['returnUrl'] = (!empty($paymentInfo['configuration']['returnUrl'])) ? $paymentInfo['configuration']['returnUrl'] : config('noon_payment.jisr.return_url');
+            $paymentInfo['configuration']['returnUrl'] = (!empty($paymentInfo['configuration']['returnUrl'])) ? $paymentInfo['configuration']['returnUrl'] : "ibrahim";
             // Options for payment action are (AUTHORIZE - SALE)
             $paymentInfo['configuration']['paymentAction'] = (!empty($paymentInfo['configuration']['paymentAction'])) ? $paymentInfo['configuration']['paymentAction'] : "SALE";
-            // dd($paymentInfo);
-            return json_decode(CurlHelper::post(config("noon_pay.jisr.payment_api") . "order", $paymentInfo, $this->getHeaders()));
+            dd($paymentInfo);
+            return json_decode(CurlHelper::post(config("noon_payment.jisr.payment_api") . "order", $paymentInfo, $this->getHeaders()));
         }
 
         if($accountName == 'Jasarah'){
