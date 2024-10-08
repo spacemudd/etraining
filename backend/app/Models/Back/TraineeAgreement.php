2374bf3dd80d6ff1e0c75ca23fcd8578300d298f
test <ebrahim-hosny@ptc-ksa.net>
2024-10-08 13:31:45 +0300

    relation

--- backend/app/Models/Back/TraineeAgreement.php
+++ backend/app/Models/Back/TraineeAgreement.php
@@ -4,8 +4,16 @@ namespace App\Models\Back;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\EducationalLevel;
+
 
 class TraineeAgreement extends Model
 {
     use HasFactory;
+
+    public function educational_level()
+    {
+        return $this->belongsTo(EducationalLevel::class);
+    }
+    
 }
