1c1d339ec8e5e64133eda62061392192d8b05379
test <ebrahim-hosny@ptc-ksa.net>
2024-10-13 11:17:09 +0300

    aa

--- backend/app/Exports/SomeCompaniesExport.php
+++ backend/app/Exports/SomeCompaniesExport.php
@@ -8,25 +8,23 @@ use Maatwebsite\Excel\Concerns\WithHeadings;
 
 class SomeCompaniesExport implements FromCollection, WithHeadings
 {
-    protected $traineeGroupId;
-
-    // public function __construct($traineeGroupId)
-    // {
-    //     $this->traineeGroupId = $traineeGroupId;
-    // }
-
     public function collection()
     {
-        return Trainee::where('trainee_group_id','72875db8-a734-4631-a579-a7ebb1312b87')
-            ->with('company') 
+        return Trainee::where('trainee_group_id', '72875db8-a734-4631-a579-a7ebb1312b87')
+            ->with('company')
             ->get()
-            ->pluck('company.name_ar') 
-            ->unique(); 
+            ->map(function ($trainee) {
+                return [
+                    'company_name' => optional($trainee->company)->name_ar, 
+                ];
+            })
+            ->unique('company_name');
     }
 
     public function headings(): array
     {
-        return ['Company Name']; 
+        return ['Company Name'];
     }
 }
+
 
