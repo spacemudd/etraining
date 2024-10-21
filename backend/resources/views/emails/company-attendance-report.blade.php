76a2965a9e252089dd2bd2ed90bb14577611f51e
test <ebrahim-hosny@ptc-ksa.net>
2024-10-21 08:48:03 +0300

    test approve

--- backend/resources/views/emails/company-attendance-report.blade.php
+++ backend/resources/views/emails/company-attendance-report.blade.php
@@ -1,12 +1,13 @@
 @component($report->company->is_ptc_net ? 'vendor.mail.html.messageptcnet' : 'vendor.mail.html.message')
 
-شكرا لكم،
+شكراً لكم،
 
 مرفق لكم كشف الحضور والانصراف للمتدربين.
 
-
 مع تحياتنا،
 
-<h1>{{$report->id}}</h1>
+<a href="/company-attendance-reports/approve/{{ $report->id }}" style="display:inline-block; padding: 10px 15px; background-color: #4CAF50; color: white; text-decoration: none; border-radius: 5px;">
+    الاعتماد الآن
+</a>
 
 @endcomponent
