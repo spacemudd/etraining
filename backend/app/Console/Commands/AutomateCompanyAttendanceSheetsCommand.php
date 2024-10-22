dca7bcd088f85706133cda9279f447c2a386d779
shafiqalshaar <shafiqalshaar@gmail.com>
2024-10-22 15:26:40 +0300

    Revert "remove star"
    This reverts commit f2dadcd22e8843ea63a5df6c1e0db44b609692dd.

--- backend/app/Console/Commands/AutomateCompanyAttendanceSheetsCommand.php
+++ backend/app/Console/Commands/AutomateCompanyAttendanceSheetsCommand.php
@@ -15,7 +15,7 @@ class AutomateCompanyAttendanceSheetsCommand extends Command
 {
     /**
      * The name and signature of the console command.
-     *
+     *///
      * @var string
      */
     protected $signature = 'company-attendance-sheets:start';
@@ -36,7 +36,6 @@ class AutomateCompanyAttendanceSheetsCommand extends Command
     {
         parent::__construct();
     }
-}
 
     /**
      * Execute the console command.
