40a5651f6e30dac31ec13dc698cc089ae6e78f49
test <ebrahim-hosny@ptc-ksa.net>
2024-11-12 16:20:45 +0300

    return

--- backend/app/Exports/SomeCompaniesExport.php
+++ backend/app/Exports/SomeCompaniesExport.php
@@ -10,17 +10,34 @@ class SomeCompaniesExport implements FromCollection, WithHeadings
 {
     public function collection()
     {
-        return Trainee::whereNull('deleted_at')
-            ->whereNull('suspended_at')
-            ->orderBy('created_at', 'desc')
-            ->get(['name', 'phone as phone_number']);
+        return Trainee::withTrashed()
+            ->where('city_id', 'd4fb0162-81ec-4b17-812a-06c7c4306cb5')
+            ->whereBetween('created_at', ['2021-01-01 00:00:00', '2023-12-31 23:59:59'])
+            ->where(function($q) {
+                $q->whereNotNull('suspended_at')
+                  ->orWhereNotNull('deleted_at');
+            })
+            ->orderBy('created_at', 'desc')  
+            ->get()
+            ->map(function($trainee) {
+                return [
+                    'name' => $trainee->name,
+                    'identity_number' => $trainee->identity_number,
+                    'phone_number' => $trainee->phone,
+                    'deleted_at' => $trainee->deleted_at,
+                    'suspended_at' => $trainee->suspended_at,
+                ];
+            });
     }
 
     public function headings(): array
     {
         return [
             'اسم المتدرب',
+            'رقم الهوية',
             'رقم الجوال',
+            'تاريخ الحذف',
+            'تاريخ الإيقاف',
         ];
     }
 }
