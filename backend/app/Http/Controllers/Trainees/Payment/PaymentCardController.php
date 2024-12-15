5765ee3baf6f9194627c302d6bb5d093b69b7f1f
test <ebrahim-hosny@ptc-ksa.net>
2024-12-15 11:13:28 +0300

    log info

--- backend/app/Http/Controllers/Trainees/Payment/PaymentCardController.php
+++ backend/app/Http/Controllers/Trainees/Payment/PaymentCardController.php
@@ -15,6 +15,7 @@ use App\Services\NoonService;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
 use Mail;
 
@@ -76,7 +77,7 @@ class PaymentCardController extends Controller
     public function storeNoonReceipt(Request $request)
     {
         $order = $this->paymentService->getOrder($request->orderId, 5676); // try finding the order in Jasarah
-        dd($order);
+        Log::info($order);
         if ($order->resultCode === 5021 || $order->resultCode === 19089) { // 5021 is bad request in Noon (not found in Jasarah)
             $order = $this->paymentService->getOrder($request->orderId, 0); // try finding the order in Jisr
         }
