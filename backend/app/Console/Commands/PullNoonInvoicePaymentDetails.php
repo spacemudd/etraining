f0c3692983f0c795e47e7b5de5d7ace180484a5a
shafiqalshaar <shafiqalshaar@gmail.com>
2024-12-22 13:54:43 +0300

    Fix url

--- backend/app/Console/Commands/PullNoonInvoicePaymentDetails.php
+++ backend/app/Console/Commands/PullNoonInvoicePaymentDetails.php
@@ -144,7 +144,7 @@ class PullNoonInvoicePaymentDetails extends Command
 
 
 
-        $url = 'https://prod.jisr-ksa.com/noon';
+        $url = 'https://app.jisr-ksa.com/noon';
 
         foreach ($invoices as $ok => $in) {
             $response = Http::post($url, json_decode('{"orderId": '.$in.',"orderStatus": "CAPTURED","eventType": "Sale","eventId": "da42a2e9-397b-4d50-b339-53e9d751a9e0","timeStamp": "2024-12-15T08:56:38.5732066Z","signature": "ZDYoQ+punJ8llb6ZwkG3XW4KIfORIH0DPRyU1Nxip5q4s/Uy8aqU5FmvqTf0HNRtjeohp0sdQaBeO55FslcYUA=="}', true));
