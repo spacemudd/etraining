419faee6f335d3661b70a2596cea17b57ed0c6f4
test <ebrahim-hosny@ptc-ksa.net>
2025-01-20 15:31:15 +0300

    npm run prod

--- backend/resources/js/Components/CourseBatchesPagination.vue
+++ backend/resources/js/Components/CourseBatchesPagination.vue
@@ -64,13 +64,13 @@
                             </tbody>
                             
                         </table>
-                              <!-- <button
+                              <button
                              
                                 @click="checkCertificateEligibility(batch.id)"
                                 class="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 mt-2"
                                 >
                                 استحقاق الشهادات بعد إنتهاء الدورة
-                            </button> -->
+                            </button>
 
                     </div>
                     <div class="col-span-1"></div>
