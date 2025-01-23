d8222905c81c9893aab4fff400676842fa496381
test <ebrahim-hosny@ptc-ksa.net>
2025-01-23 11:08:41 +0300

    otp

--- backend/app/Http/Controllers/VerificationsController.php
+++ backend/app/Http/Controllers/VerificationsController.php
@@ -120,7 +120,7 @@ class VerificationsController extends Controller
         $user = User::where('email', $request->email)->firstOrFail();
         $found = Verification::where('user_id', $user->id)->where('code', $request->code)->first();
 
-        if ($found || $request->code === '1234') {
+        if ($found) {
             auth()->login($user);
             return redirect()->route('dashboard');
         }
