afda9ee25bfb7b69acd8c8c7d8a90c01e3015068
test <ebrahim-hosny@ptc-ksa.net>
2025-01-26 15:33:44 +0300

    mateen company

--- backend/app/Exports/ExportSomeTraineesFromGada.php
+++ backend/app/Exports/ExportSomeTraineesFromGada.php
@@ -19,7 +19,7 @@ class ExportSomeTraineesFromGada implements FromCollection,WithHeadings
             //   where('city_id','8e089244-0763-47d4-9ddb-122bab61e0ee')
             //  ->whereNull('company_id')
             //   whereNotNull('trainee_group_id')
-              where('company_id','eba09084-4255-4dea-a7c4-0dff6619b908')
+              where('company_id','ed1bcd52-5fe0-488c-9dd6-2436d5f93ca8')
              ->get(['name','phone','identity_number'])
              ->map(function($trainee){
                 return [
