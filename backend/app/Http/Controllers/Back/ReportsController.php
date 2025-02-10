bc677a28f13c989dae6b043091e464f85ff1163f
test <ebrahim-hosny@ptc-ksa.net>
2025-02-10 10:29:06 +0300

    all company certificates

--- backend/app/Http/Controllers/Back/ReportsController.php
+++ backend/app/Http/Controllers/Back/ReportsController.php
@@ -151,112 +151,117 @@ class ReportsController extends Controller
         return Excel::download(new TraineesWithoutInvoicesExport($data), now()->format('Y-m-d').'-traineees-without-invoices.xlsx');
        
     }
-
     public function formCompanyCertificateseGenerateReport(Request $request)
-{
-    // dd($request->input('courseId.id'));
-    $course = Course::find($request->input('courseId.id'));
-
-    if (!$course) {
-        return response()->json(['error' => 'Course not found'], 404);
-    }
-
-    $courseName = $course->name_ar;
-
-    $courses = Course::where('name_ar', $courseName)->get();
-
-    $results = [];
-    ini_set('memory_limit', '512M');
-    set_time_limit(300);
-    $companyId=$request->input('companyId.id');
+    {
+        $course = Course::find($request->input('courseId.id'));
     
-    foreach ($courses as $course) {
-        $batches = $course->batches;
-
-        foreach ($batches as $batch) {
-            $courseEndDate = Carbon::parse($batch->ends_at);
-            $startOfMonth = $courseEndDate->copy()->startOfMonth();
-            $daysDifference = $courseEndDate->diffInDays($startOfMonth);
-
-            if ($daysDifference >= 15) {
-                $targetMonth = $courseEndDate->month;
-                $targetYear = $courseEndDate->year;
-            } else {
-                $targetMonth = $courseEndDate->subMonth()->month;
-                $targetYear = $courseEndDate->year;
-            }
-
-            $startOfTargetMonth = Carbon::createFromDate($targetYear, $targetMonth, 1)->startOfMonth();
-            $endOfTargetMonth = Carbon::createFromDate($targetYear, $targetMonth, 1)->endOfMonth();
-
-            $totalSessionsCount = $batch->course_batch_sessions()->count();
-
-            $batch->trainee_group->trainees()->where('company_id',$companyId)->chunk(100, function ($traineesChunk) use (
-                &$results,
-                $batch,
-                $totalSessionsCount,
-                $startOfTargetMonth,
-                $endOfTargetMonth,
-                $companyId
-            ) {
-                foreach ($traineesChunk as $trainee) {
-                    $attendanceRecords = $trainee->attendanceReportRecords()
-                        ->where('course_batch_id', $batch->id)
-                        ->get()
-                        ->unique('course_batch_session_id');
-
-                    $presentCount = $attendanceRecords->whereIn('status', [1, 2, 3])->count();
-                    $absentCount = $attendanceRecords->where('status', 0)->count();
-
-                    $attendancePercentage = $totalSessionsCount > 0 ? ($presentCount / $totalSessionsCount) * 100 : 0;
-
-                    $invoice = Invoice::where('trainee_id', $trainee->id)
-                        ->where('company_id', $companyId)
-                        ->where(function ($query) use ($startOfTargetMonth, $endOfTargetMonth) {
+        if (!$course) {
+            return response()->json(['error' => 'Course not found'], 404);
+        }
+    
+        $courseName = $course->name_ar;
+        $courses = Course::where('name_ar', $courseName)->get();
+    
+        $results = [];
+        ini_set('memory_limit', '512M');
+        set_time_limit(300);
+    
+        $companyId = $request->input('companyId.id'); 
+        
+        foreach ($courses as $course) {
+            $batches = $course->batches;
+    
+            foreach ($batches as $batch) {
+                $courseEndDate = Carbon::parse($batch->ends_at);
+                $startOfMonth = $courseEndDate->copy()->startOfMonth();
+                $daysDifference = $courseEndDate->diffInDays($startOfMonth);
+    
+                if ($daysDifference >= 15) {
+                    $targetMonth = $courseEndDate->month;
+                    $targetYear = $courseEndDate->year;
+                } else {
+                    $targetMonth = $courseEndDate->subMonth()->month;
+                    $targetYear = $courseEndDate->year;
+                }
+    
+                $startOfTargetMonth = Carbon::createFromDate($targetYear, $targetMonth, 1)->startOfMonth();
+                $endOfTargetMonth = Carbon::createFromDate($targetYear, $targetMonth, 1)->endOfMonth();
+    
+                $totalSessionsCount = $batch->course_batch_sessions()->count();
+    
+                $traineesQuery = $batch->trainee_group->trainees();
+    
+                if ($companyId) {
+                    $traineesQuery->where('company_id', $companyId);
+                }
+    
+                $traineesQuery->chunk(100, function ($traineesChunk) use (
+                    &$results,
+                    $batch,
+                    $totalSessionsCount,
+                    $startOfTargetMonth,
+                    $endOfTargetMonth,
+                    $companyId
+                ) {
+                    foreach ($traineesChunk as $trainee) {
+                        $attendanceRecords = $trainee->attendanceReportRecords()
+                            ->where('course_batch_id', $batch->id)
+                            ->get()
+                            ->unique('course_batch_session_id');
+    
+                        $presentCount = $attendanceRecords->whereIn('status', [1, 2, 3])->count();
+                        $absentCount = $attendanceRecords->where('status', 0)->count();
+    
+                        $attendancePercentage = $totalSessionsCount > 0 ? ($presentCount / $totalSessionsCount) * 100 : 0;
+    
+                        $invoiceQuery = Invoice::where('trainee_id', $trainee->id);
+    
+                        if ($companyId) {
+                            $invoiceQuery->where('company_id', $companyId);
+                        }
+    
+                        $invoice = $invoiceQuery->where(function ($query) use ($startOfTargetMonth, $endOfTargetMonth) {
                             $query->whereBetween('from_date', [$startOfTargetMonth, $endOfTargetMonth])
                                 ->orWhereBetween('to_date', [$startOfTargetMonth, $endOfTargetMonth])
                                 ->orWhere(function ($query) use ($startOfTargetMonth, $endOfTargetMonth) {
                                     $query->where('from_date', '<=', $startOfTargetMonth)
                                         ->where('to_date', '>=', $endOfTargetMonth);
                                 });
-                        })
-                        ->first();
-
-                    $invoiceStatus = $invoice ? $invoice->status_formatted : 'لا توجد فاتورة';
-                    $paidDate = $invoice ? $invoice->paid_at : '';
-                    $invoiceFromDate = $invoice ? $invoice->from_date : '';
-                    $invoiceToDate = $invoice ? $invoice->to_date : '';
-
-                    if (isset($trainee->name)) {
-                        $results[] = [
-                            'paid_date' => $paidDate,
-                            'invoice_to_date' => $invoiceToDate,
-                            'invoice_from_date' => $invoiceFromDate,
-                            'invoice_status' => $invoiceStatus,
-                            'attendance_percentage' => round($attendancePercentage, 2) . ' %',
-                            'present_count' => $presentCount,
-                            'absent_count' => $absentCount,
-                            'email' => $trainee->email,
-                            'phone' => $trainee->phone,
-                            'identity_number' => $trainee->identity_number,
-                            'trainee_name' => $trainee->name,
-                        ];
-                    }
-                }
-            });
-        }
-    }
-
-    $results = collect($results)->sortByDesc(function ($trainee) {
-        $attendanceNumeric = floatval(str_replace(' %', '', $trainee['attendance_percentage']));
-        return ($attendanceNumeric >= 50 && $trainee['invoice_status'] == 'مدفوع') ? 1 : 0;
-    })->values()->toArray();
-
-
-    return Excel::download(new TraineeAttendanceExportByGroup($results), 'trainee_attendance_by_course.xlsx');
+                        })->first();
     
-}
-
+                        $invoiceStatus = $invoice ? $invoice->status_formatted : 'لا توجد فاتورة';
+                        $paidDate = $invoice ? $invoice->paid_at : '';
+                        $invoiceFromDate = $invoice ? $invoice->from_date : '';
+                        $invoiceToDate = $invoice ? $invoice->to_date : '';
+    
+                        if (isset($trainee->name)) {
+                            $results[] = [
+                                'paid_date' => $paidDate,
+                                'invoice_to_date' => $invoiceToDate,
+                                'invoice_from_date' => $invoiceFromDate,
+                                'invoice_status' => $invoiceStatus,
+                                'attendance_percentage' => round($attendancePercentage, 2) . ' %',
+                                'present_count' => $presentCount,
+                                'absent_count' => $absentCount,
+                                'email' => $trainee->email,
+                                'phone' => $trainee->phone,
+                                'identity_number' => $trainee->identity_number,
+                                'trainee_name' => $trainee->name,
+                            ];
+                        }
+                    }
+                });
+            }
+        }
+    
+        $results = collect($results)->sortByDesc(function ($trainee) {
+            $attendanceNumeric = floatval(str_replace(' %', '', $trainee['attendance_percentage']));
+            return ($attendanceNumeric >= 50 && $trainee['invoice_status'] == 'مدفوع') ? 1 : 0;
+        })->values()->toArray();
+    
+        return Excel::download(new TraineeAttendanceExportByGroup($results), 'trainee_attendance_by_course.xlsx');
+    }
+    
     
 
 }
