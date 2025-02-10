f7c9c8f32a7f0bb05525adaca24a33037b7c8eed
test <ebrahim-hosny@ptc-ksa.net>
2025-02-10 16:39:25 +0300

    export trainees without companies

--- backend/app/Exports/ExportSomeTraineesFromGada.php
+++ backend/app/Exports/ExportSomeTraineesFromGada.php
@@ -16,13 +16,15 @@ class ExportSomeTraineesFromGada implements FromCollection,WithHeadings
     public function collection()
     {
         return Trainee::
+            whereDoesntHave('company')
             //   where('city_id','8e089244-0763-47d4-9ddb-122bab61e0ee')
             //  ->whereNull('company_id')
             //   whereNotNull('trainee_group_id')
-              where('company_id','3b5b63f7-223a-404b-bb0e-90d7a1af1ff5')
-             ->get(['name','phone','identity_number'])
+            //   where('company_id','3b5b63f7-223a-404b-bb0e-90d7a1af1ff5')
+             ->get(['name','phone','identity_number','email'])
              ->map(function($trainee){
                 return [
+                    'email' => $trainee->email,
                     'identity_number' => $trainee->identity_number,
                     'phone' => $trainee->clean_phone,
                     'name' => $trainee->name,
@@ -33,6 +35,7 @@ class ExportSomeTraineesFromGada implements FromCollection,WithHeadings
     public function headings(): array
     {
         return [
+            'الإيميل',
             'رقم الهوية',
             'رقم الجوال',
             'الإسم'
