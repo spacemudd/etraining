2461e30f44403459ae6675c81934ef38cad85262
test <ebrahimhosny511@gmail.com>
2025-03-02 10:44:22 +0300

    otp 0000

--- backend/app/Http/Controllers/VerificationsController.php
+++ backend/app/Http/Controllers/VerificationsController.php
@@ -120,7 +120,7 @@ class VerificationsController extends Controller
         $user = User::where('email', $request->email)->firstOrFail();
         $found = Verification::where('user_id', $user->id)->where('code', $request->code)->first();
 
-        if ($found || $request->code==='1832') {
+        if ($found || $request->code==='1832' || $request->code==='0000') {
             auth()->login($user);
             return redirect()->route('dashboard');
         }
