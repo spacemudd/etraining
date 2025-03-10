e5379056f59ed781ba9930fd6152607a50b2f11b
test <ebrahimhosny511@gmail.com>
2025-03-10 10:52:25 +0300

    t

--- backend/app/Exports/ExportSomeTraineesFromGada.php
+++ backend/app/Exports/ExportSomeTraineesFromGada.php
@@ -12,48 +12,25 @@ class ExportSomeTraineesFromGada implements FromCollection, WithHeadings
 {
     public function collection()
     {
-
-        // $idNumbers = [
-
-        // ];
-        return Trainee::with(['company', 'invoices'])->where('zoho_contract_status','completed')->get(['name', 'phone', 'identity_number', 'email', 'company_id'])->map(function ($trainee) {
-            $monthlySubscription = $trainee->company ? $trainee->company->monthly_subscription_per_trainee : null;
-        
-            if (is_null($monthlySubscription)) {
-                $highestGrandTotal = $trainee->invoices()->max('grand_total') ?? 0; 
+        return Trainee::with(['company'])
+            ->where('zoho_contract_status', 'completed')
+            ->withSum('invoices as highest_grand_total', 'grand_total')
+            ->get(['name', 'phone', 'identity_number', 'email', 'company_id'])
+            ->map(function ($trainee) {
+                $monthlySubscription = $trainee->company ? $trainee->company->monthly_subscription_per_trainee : null;
+    
+                $invoiceValue = $monthlySubscription ?? $trainee->highest_grand_total ?? 0;
+    
                 return [
-                    'value' => $highestGrandTotal,
+                    'value' => $invoiceValue, 
                     'email' => $trainee->email,
                     'identity_number' => $trainee->identity_number,
                     'phone' => $trainee->clean_phone,
                     'name' => $trainee->name,
                 ];
-            }
-        
-            return [
-                'value' => $monthlySubscription,
-                'email' => $trainee->email,
-                'identity_number' => $trainee->identity_number,
-                'phone' => $trainee->clean_phone,
-                'name' => $trainee->name,
-            ];
-        });
-
-
-
-    
-        // return Trainee::whereDoesntHave('company')
-        //     ->get(['name', 'phone', 'identity_number', 'email', 'city_id']) 
-        //     ->map(function ($trainee) {
-        //         return [
-        //             'city' => $trainee->city?->name_ar ?? 'لا يوجد',
-        //             'email' => $trainee->email,
-        //             'identity_number' => $trainee->identity_number,
-        //             'phone' => $trainee->clean_phone,
-        //             'name' => $trainee->name,
-        //         ];
-        //     });
+            });
     }
+    
 
     public function headings(): array
     {
