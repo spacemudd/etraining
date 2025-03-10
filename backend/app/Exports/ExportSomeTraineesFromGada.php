2507a51bfe04b3210302aac8c72de8dfce8ac572
test <ebrahimhosny511@gmail.com>
2025-03-10 11:06:48 +0300

    t

--- backend/app/Exports/ExportSomeTraineesFromGada.php
+++ backend/app/Exports/ExportSomeTraineesFromGada.php
@@ -11,27 +11,27 @@ use Carbon\Carbon;
 class ExportSomeTraineesFromGada implements FromCollection, WithHeadings
 {
     public function collection()
-{
-    return Trainee::with(['company', 'invoices' => function ($query) {
-            $query->orderBy('grand_total', 'desc')->limit(1); 
-        }])
-        ->where('zoho_contract_status', 'completed')
-        ->get(['name', 'phone', 'identity_number', 'email', 'company_id'])
-        ->map(function ($trainee) {
-            $monthlySubscription = $trainee->company ? $trainee->company->monthly_subscription_per_trainee : null;
-            $highestInvoice = $trainee->invoices->first(); 
-
-            $invoiceValue = $monthlySubscription ?? ($highestInvoice?->grand_total ?? 0);
-
-            return [
-                'value' => $invoiceValue,
-                'email' => $trainee->email,
-                'identity_number' => $trainee->identity_number,
-                'phone' => $trainee->clean_phone,
-                'name' => $trainee->name,
-            ];
-        });
-}
+    {
+        return Trainee::with(['company', 'invoices' => function ($query) {
+                $query->orderBy('grand_total', 'desc'); 
+            }])
+            ->where('zoho_contract_status', 'completed')
+            ->get(['name', 'phone', 'identity_number', 'email', 'company_id'])
+            ->map(function ($trainee) {
+                $highestInvoice = $trainee->invoices->first();
+    
+                $invoiceValue = $trainee->company?->monthly_subscription_per_trainee 
+                    ?? ($highestInvoice ? $highestInvoice->grand_total : 0);
+    
+                return [
+                    'value' => $invoiceValue, 
+                    'email' => $trainee->email,
+                    'identity_number' => $trainee->identity_number,
+                    'phone' => $trainee->phone, 
+                    'name' => $trainee->name,
+                ];
+            });
+    }
 
 
     public function headings(): array
