2d5b0e779176d28c5941d1ba48c246ef93a507c8
test <ebrahimhosny511@gmail.com>
2025-04-22 14:11:37 +0300

    .

--- backend/app/Exports/ExportSomeTraineesFromGada.php
+++ backend/app/Exports/ExportSomeTraineesFromGada.php
@@ -6,41 +6,32 @@ use App\Models\Back\Trainee;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\DB;
-
-
 
 class ExportSomeTraineesFromGada implements FromCollection, WithHeadings
 {
     public function collection()
     {
+        ini_set('max_execution_time', 300); // زيادة وقت التنفيذ
 
-        $date=Carbon::parse('2025-01-01');
-        
-        
-        
-        $trainees=Trainee::whereHas('user',function ($q) use ($date){
-            $q->where('last_login_at','<',$date);
-        })->get();
+        $date = Carbon::parse('2025-01-01');
 
+        $trainees = Trainee::with('user')
+            ->whereHas('user', function ($q) use ($date) {
+                $q->where('last_login_at', '<', $date);
+            })
+            ->get();
 
-
-        return $trainees->map(function($trainee){
+        return $trainees->map(function ($trainee) {
             return [
                 'name' => $trainee->name,
                 'identity_number' => $trainee->identity_number,
-                'email'=> $trainee->email,
+                'email' => $trainee->email,
                 'phone' => $trainee->phone,
-                'company' => optional($trainee->company)->name_ar,
-                'last_login_at'=>$trainee->user->last_login_at,
+                'company' => $trainee->company->name_ar ?? '-', // لو ما في شركة يظهر "-"
+                'last_login_at' => optional($trainee->user)->last_login_at ?? '-',
             ];
         });
-   
-
     }
-
-    
-
 
     public function headings(): array
     {
@@ -51,8 +42,6 @@ class ExportSomeTraineesFromGada implements FromCollection, WithHeadings
             'الجوال',
             'الشركة',
             'تاريخ آخر دخول',
-
         ];
     }
 }
-
