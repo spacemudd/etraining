44ef00f172c79abefdb42e8806a305dd5cb16ccd
test <ebrahimhosny511@gmail.com>
2025-06-16 09:27:07 +0300

    resignation email

--- backend/resources/views/emails/resignations.blade.php
+++ backend/resources/views/emails/resignations.blade.php
@@ -1,40 +1,35 @@
 @component('mail::resignation-message-layout')
 
-{{--السادة الكرام / {{ $resignation->company->name_ar }}--}}
+{{-- السادة الكرام / {{ $resignation->company->name_ar }} --}}
 
 @if ($resignation->trainees()->count() > 1)
 بالإشارة إلى عقد التدريب نفيدكم بأن المتدربات أدناه:
 
 @foreach ($resignation->trainees as $trainee)
-    - {{ $trainee->name }}
+- {{ $trainee->name }}
 @endforeach
 
-تم إيقافهم من البرنامج التدريبي بناء على رغبتهم وطلبهم، ومرفق لكم المستندات المستلمة من قبل المتدربات.
-
-مع تحياتنا،
+تم إيقافهم من البرنامج التدريبي بناءً على رغبتهن وطلبهن، مرفق لكم المستندات المستلمة من قبل المتدربات.
 @else
 نفيدكم بأن السيدة/ **{{ $resignation->trainees()->first()->name }}**
 
-تم إيقافها من البرنامج التدريبي بناء على رغبتها وطلبها، ومرفق لكم المستندات المستلمة . مع تحياتنا
-
-{{--<br/>--}}
-{{--<hr style="color:white;">--}}
-{{--<br/>--}}
-
-{{--يرجى الضغط أدناه لتأكيد استلامكم للمستندات المرفقة.--}}
-
-{{--@component('mail::button', ['url' => route('resignations.confirm-received', ['id' => $resignation->id])])--}}
-{{--تأكيد استلام المستندات--}}
-{{--@endcomponent--}}
-
-مع تحياتنا،
+تم إيقافها من البرنامج التدريبي بناءً على رغبتها وطلبها، مرفق لكم المستندات المستلمة.
 @endif
 
-    @slot('footer')
-        @component('mail::resignation-footer')
-            البريد الالكتروني مرسل عن طريق النظام الإلكتروني بشكل تلقائي من قبل المتدربة والمستندات المرفقة مرفوعة من قبلها وفي حالة وجود اي ملاحظة لا تترددوا بالتواصل معنا على البريد الالكتروني المعتمد في العقدة.
-            <br/>
-            © {{ date('Y') }} @lang('All rights reserved.')
-        @endcomponent
-    @endslot
+{{-- زر التأكيد اختياري إذا أردت إضافته --}}
+{{-- 
+@component('mail::button', ['url' => route('resignations.confirm-received', ['id' => $resignation->id])])
+تأكيد استلام المستندات
+@endcomponent 
+--}}
+
+مع تحياتنا،
+
+@slot('footer')
+    @component('mail::resignation-footer')
+        البريد الإلكتروني مرسل عن طريق النظام الإلكتروني بشكل تلقائي من قبل المتدربة والمستندات المرفقة مرفوعة من قبلها. وفي حال وجود أي ملاحظة لا تترددوا بالتواصل معنا على البريد الإلكتروني المعتمد في العقد.
+        <br/>
+        © {{ date('Y') }} @lang('All rights reserved.')
+    @endcomponent
+@endslot
 @endcomponent
