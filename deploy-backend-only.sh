fbe9d958be4f58a3d6b8dc5ab94825311a803225
Shafiq Alshaar <shafiqalshaar@gmail.com>
2025-10-14 17:26:32 +0300

    Backend only update API

--- deploy-backend-only.sh
+++ deploy-backend-only.sh
@@ -1,6 +1,6 @@
 docker login -u AWS -p $(aws ecr get-login-password) https://$(aws sts get-caller-identity --query 'Account' --output text).dkr.ecr.eu-central-1.amazonaws.com
 docker-compose build backend
-docker tag etraining-backend:latest 175380537962.dkr.ecr.eu-central-1.amazonaws.com/backend
-docker push 175380537962.dkr.ecr.eu-central-1.amazonaws.com/backend
+docker tag etraining-backend:latest 912413319130.dkr.ecr.eu-central-1.amazonaws.com/backend
+docker push 912413319130.dkr.ecr.eu-central-1.amazonaws.com/backend
 aws ecs update-service --cluster prod-ecs-cluster --service backend-service --force-new-deployment
 docker image prune --force
