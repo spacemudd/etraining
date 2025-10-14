21aeb1780e6a22c98b2497702a769cc7c2d15c80
Shafiq Alshaar <shafiqalshaar@gmail.com>
2025-10-14 17:38:42 +0300

    Fix deployment to correct ECR

--- deploy-backend-schedule.sh
+++ deploy-backend-schedule.sh
@@ -1,5 +1,5 @@
 docker login -u AWS -p $(aws ecr get-login-password) https://$(aws sts get-caller-identity --query 'Account' --output text).dkr.ecr.eu-central-1.amazonaws.com
 docker-compose build backend-schedule
-docker tag etraining_backend-schedule:latest 175380537962.dkr.ecr.eu-central-1.amazonaws.com/schedule
-docker push 175380537962.dkr.ecr.eu-central-1.amazonaws.com/schedule
+docker tag etraining_backend-schedule:latest 912413319130.dkr.ecr.eu-central-1.amazonaws.com/schedule
+docker push 912413319130.dkr.ecr.eu-central-1.amazonaws.com/schedule
 aws ecs update-service --cluster prod-ecs-cluster --service scheduler --force-new-deployment
--- deploy-backend-worker.sh
+++ deploy-backend-worker.sh
@@ -1,5 +1,5 @@
 docker login -u AWS -p $(aws ecr get-login-password) https://$(aws sts get-caller-identity --query 'Account' --output text).dkr.ecr.eu-central-1.amazonaws.com
 docker-compose build backend-worker
-docker tag etraining_backend-worker:latest 175380537962.dkr.ecr.eu-central-1.amazonaws.com/worker
-docker push 175380537962.dkr.ecr.eu-central-1.amazonaws.com/worker
+docker tag etraining_backend-worker:latest 912413319130.dkr.ecr.eu-central-1.amazonaws.com/worker
+docker push 912413319130.dkr.ecr.eu-central-1.amazonaws.com/worker
 aws ecs update-service --cluster prod-ecs-cluster --service worker-service --force-new-deployment
