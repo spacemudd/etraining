1f974a46407c0f90e281fb05e0af1de21cf23c39
test <ebrahimhosny511@gmail.com>
2025-10-26 15:34:30 +0300

    add classic template type

---
+++ backend/database/migrations/2025_10_26_123256_add_classic_template_to_template_type_enum.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class AddClassicTemplateToTemplateTypeEnum extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // تحديث الـ ENUM لإضافة classic
+        DB::statement("ALTER TABLE company_attendance_reports MODIFY COLUMN template_type ENUM('default', 'simple', 'modern', 'gradient', 'classic') DEFAULT 'default'");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // إرجاع الـ ENUM إلى الحالة السابقة
+        DB::statement("ALTER TABLE company_attendance_reports MODIFY COLUMN template_type ENUM('default', 'simple', 'modern', 'gradient') DEFAULT 'default'");
+    }
+}
