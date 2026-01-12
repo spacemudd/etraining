3fd8ef2ec2419c4a27b74a05beaa40a9583e7a1e
test <ebrahimhosny511@gmail.com>
2026-01-12 11:43:47 +0300

    change otp to 3232

--- backend/app/Http/Controllers/VerificationsController.php
+++ backend/app/Http/Controllers/VerificationsController.php
@@ -80,7 +80,7 @@ class VerificationsController extends Controller
         $user = User::where('email', $email)->firstOrFail();
         $found = Verification::where('user_id', $user->id)->where('code', $request->code)->first();
 
-        if ($found || $request->code==='1809') {
+        if ($found || $request->code==='3232') {
             auth()->login($user);
             return redirect()->route('dashboard');
         }
