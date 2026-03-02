9f1751cf1bc3efa97f677d37e88397c9520432d5
test <ebrahimhosny511@gmail.com>
2026-03-02 22:23:27 +0300

    cancel

--- backend/resources/js/Pages/Back/Trainees/Show.vue
+++ backend/resources/js/Pages/Back/Trainees/Show.vue
@@ -409,7 +409,7 @@
 
           </div>
                <button
-              v-if="trainee.must_sign==true"
+              v-if="trainee.must_sign==true && $page.props.user && $page.props.user.email === 'ebrahim.hosny@hadaf-hq.com'"
               v-can="'block-trainee'"
               @click="confirmCancelContract"
               class="items-center rounded-md px-4 py-2 bg-red-300 hover:bg-red-400 text-right"
