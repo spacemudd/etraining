359edc605d9150a145745f600310216bffa53c99
test <ebrahimhosny511@gmail.com>
2026-06-03 16:33:48 +0300

    att mon 5

--- backend/app/Console/Commands/AutomateCompanyAttendanceSheetsCommand.php
+++ backend/app/Console/Commands/AutomateCompanyAttendanceSheetsCommand.php
@@ -43,8 +43,8 @@ class AutomateCompanyAttendanceSheetsCommand extends Command
      */
     public function handle()
     {
-        $from_date = Carbon::parse('2026-04-01')->startOfDay();
-        $to_date = Carbon::parse('2026-04-29')->endOfDay();
+        $from_date = Carbon::parse('2026-05-01')->startOfDay();
+        $to_date = Carbon::parse('2026-05-30')->endOfDay();
         $this->createReportsBasedOnTraineedInvoiced($from_date, $to_date);
         return 1;
     }
@@ -57,7 +57,7 @@ class AutomateCompanyAttendanceSheetsCommand extends Command
 
         $company_id = 'beff7800-b94b-4831-8044-8cdcb152087a';
 
-        $select_invoices_from = ['2026-03-01', '2026-03-31'];
+        $select_invoices_from = ['2026-04-01', '2026-04-30'];
 
         //foreach ($companies as $company_id) {
             $count = Company::with('invoices')
